unageek/graphest

commit 4c863d510e37f5150214180415cdb083a08e88d0

Refactor id/site assignment (#24)

src/visit.rs
@@ -1,6 +1,7 @@
 use crate::{
     ast::{
         BinaryOp, Form, FormId, FormKind, NaryOp, RelOp, Term, TermId, TermKind, UnaryOp, VarSet,
+        UNINIT_FORM_ID, UNINIT_TERM_ID,
     },
     interval_set::{Site, TupperIntervalSet},
     rel::{StaticForm, StaticFormKind, StaticTerm, StaticTermKind},
@@ -466,28 +467,28 @@ impl VisitMut for UpdateMetadata {
     }
 }
 
-type SiteMap = HashMap<TermId, Option<Site>>;
+type SiteMap = HashMap<TermId, Site>;
 
 /// Assigns [`TermId`]s to all of the terms, and assigns [`FormId`]s to the atomic formulas
 /// so that they serve as indices in [`EvalResult`][`crate::eval_result::EvalResult`].
-pub struct AssignIdStage1<'a> {
+pub struct AssignIdStage1 {
+    next_form_id: FormId,
     next_term_id: TermId,
     next_site: u8,
     site_map: SiteMap,
-    visited_terms: HashSet<&'a Term>,
-    next_form_id: FormId,
-    visited_forms: HashSet<&'a Form>,
+    visited_forms: HashSet<*const Form>,
+    visited_terms: HashSet<*const Term>,
 }
 
-impl<'a> AssignIdStage1<'a> {
+impl AssignIdStage1 {
     pub fn new() -> Self {
         AssignIdStage1 {
+            next_form_id: 0,
             next_term_id: 0,
             next_site: 0,
             site_map: HashMap::new(),
-            visited_terms: HashSet::new(),
-            next_form_id: 0,
             visited_forms: HashSet::new(),
+            visited_terms: HashSet::new(),
         }
     }
 
@@ -517,46 +518,43 @@ impl<'a> AssignIdStage1<'a> {
     }
 }
 
-impl<'a> Visit<'a> for AssignIdStage1<'a> {
-    fn visit_term(&mut self, t: &'a Term) {
-        traverse_term(self, t);
+impl VisitMut for AssignIdStage1 {
+    fn visit_term_mut(&mut self, t: &mut Term) {
+        traverse_term_mut(self, t);
 
-        match self.visited_terms.get(t) {
+        match self.visited_terms.get(&(t as *const Term)) {
             Some(visited) => {
-                let id = visited.id.get();
-                t.id.set(id);
+                let id = unsafe { (**visited).id };
+                t.id = id;
 
-                if let Some(site) = self.site_map.get_mut(&id) {
-                    if site.is_none() && self.next_site <= Site::MAX {
-                        *site = Some(Site::new(self.next_site));
-                        self.next_site += 1;
-                    }
+                if !self.site_map.contains_key(&id)
+                    && Self::term_can_perform_cut(&t.kind)
+                    && self.next_site <= Site::MAX
+                {
+                    self.site_map.insert(id, Site::new(self.next_site));
+                    self.next_site += 1;
                 }
             }
             _ => {
-                let id = self.next_term_id;
-                t.id.set(id);
+                assert!(self.next_term_id != UNINIT_TERM_ID);
+                t.id = self.next_term_id;
                 self.next_term_id += 1;
-
-                if Self::term_can_perform_cut(&t.kind) {
-                    self.site_map.insert(id, None);
-                }
-
                 self.visited_terms.insert(t);
             }
         }
     }
 
-    fn visit_form(&mut self, f: &'a Form) {
-        traverse_form(self, f);
+    fn visit_form_mut(&mut self, f: &mut Form) {
+        traverse_form_mut(self, f);
 
         if let FormKind::Atomic(_, _, _) = f.kind {
-            match self.visited_forms.get(f) {
+            match self.visited_forms.get(&(f as *const Form)) {
                 Some(visited) => {
-                    f.id.set(visited.id.get());
+                    f.id = unsafe { (**visited).id };
                 }
                 _ => {
-                    f.id.set(self.next_form_id);
+                    assert!(self.next_form_id != UNINIT_FORM_ID);
+                    f.id = self.next_form_id;
                     self.next_form_id += 1;
                     self.visited_forms.insert(f);
                 }
@@ -565,45 +563,37 @@ impl<'a> Visit<'a> for AssignIdStage1<'a> {
     }
 }
 
-/// Assigns [`Site`]s to the terms if they are necessary for branch cut tracking,
-/// and assigns [`TermId`]s to the non-atomic formulas.
-pub struct AssignIdStage2<'a> {
+/// Assigns [`TermId`]s to the non-atomic formulas.
+pub struct AssignIdStage2 {
+    next_form_id: FormId,
     next_term_id: TermId,
     site_map: SiteMap,
-    next_form_id: FormId,
-    visited_forms: HashSet<&'a Form>,
+    visited_forms: HashSet<*const Form>,
 }
 
-impl<'a> AssignIdStage2<'a> {
-    pub fn new(stage1: AssignIdStage1<'a>) -> Self {
+impl AssignIdStage2 {
+    pub fn new(stage1: AssignIdStage1) -> Self {
         AssignIdStage2 {
+            next_form_id: stage1.next_form_id,
             next_term_id: stage1.next_term_id,
             site_map: stage1.site_map,
-            next_form_id: stage1.next_form_id,
             visited_forms: HashSet::new(),
         }
     }
 }
 
-impl<'a> Visit<'a> for AssignIdStage2<'a> {
-    fn visit_term(&mut self, t: &'a Term) {
-        traverse_term(self, t);
-
-        if let Some(site) = self.site_map.get(&t.id.get()) {
-            t.site.set(*site);
-        }
-    }
-
-    fn visit_form(&mut self, f: &'a Form) {
-        traverse_form(self, f);
+impl VisitMut for AssignIdStage2 {
+    fn visit_form_mut(&mut self, f: &mut Form) {
+        traverse_form_mut(self, f);
 
         if !matches!(f.kind, FormKind::Atomic(_, _, _)) {
-            match self.visited_forms.get(f) {
+            match self.visited_forms.get(&(f as *const Form)) {
                 Some(visited) => {
-                    f.id.set(visited.id.get());
+                    f.id = unsafe { (**visited).id };
                 }
                 _ => {
-                    f.id.set(self.next_form_id);
+                    assert!(self.next_form_id != UNINIT_FORM_ID);
+                    f.id = self.next_form_id;
                     self.next_form_id += 1;
                     self.visited_forms.insert(f);
                 }
@@ -614,6 +604,7 @@ impl<'a> Visit<'a> for AssignIdStage2<'a> {
 
 /// Collects [`StaticTerm`]s and [`StaticForm`]s in the topological order.
 pub struct CollectStatic {
+    site_map: SiteMap,
     terms: Vec<Option<StaticTerm>>,
     forms: Vec<Option<StaticForm>>,
 }
@@ -621,6 +612,7 @@ pub struct CollectStatic {
 impl CollectStatic {
     pub fn new(stage2: AssignIdStage2) -> Self {
         Self {
+            site_map: stage2.site_map,
             terms: vec![None; stage2.next_term_id as usize],
             forms: vec![None; stage2.next_form_id as usize],
         }
@@ -640,19 +632,19 @@ impl<'a> Visit<'a> for CollectStatic {
         use TermKind::*;
         traverse_term(self, t);
 
-        let i = t.id.get() as usize;
+        let i = t.id as usize;
         if self.terms[i].is_none() {
             self.terms[i] = Some(StaticTerm {
-                site: t.site.get(),
+                site: self.site_map.get(&t.id).copied(),
                 kind: match &t.kind {
                     Constant(x) => StaticTermKind::Constant(x.clone()),
                     Var(x) if x == "x" => StaticTermKind::X,
                     Var(x) if x == "y" => StaticTermKind::Y,
-                    Unary(op, x) => StaticTermKind::Unary(*op, x.id.get()),
-                    Binary(op, x, y) => StaticTermKind::Binary(*op, x.id.get(), y.id.get()),
-                    Pown(x, y) => StaticTermKind::Pown(x.id.get(), *y),
+                    Unary(op, x) => StaticTermKind::Unary(*op, x.id),
+                    Binary(op, x, y) => StaticTermKind::Binary(*op, x.id, y.id),
+                    Pown(x, y) => StaticTermKind::Pown(x.id, *y),
                     Nary(op, xs) => {
-                        StaticTermKind::Nary(*op, Box::new(xs.iter().map(|x| x.id.get()).collect()))
+                        StaticTermKind::Nary(*op, Box::new(xs.iter().map(|x| x.id).collect()))
                     }
                     Var(_) | Uninit => panic!(),
                 },
@@ -665,13 +657,13 @@ impl<'a> Visit<'a> for CollectStatic {
         use FormKind::*;
         traverse_form(self, f);
 
-        let i = f.id.get() as usize;
+        let i = f.id as usize;
         if self.forms[i].is_none() {
             self.forms[i] = Some(StaticForm {
                 kind: match &f.kind {
-                    Atomic(op, x, y) => StaticFormKind::Atomic(*op, x.id.get(), y.id.get()),
-                    And(x, y) => StaticFormKind::And(x.id.get(), y.id.get()),
-                    Or(x, y) => StaticFormKind::Or(x.id.get(), y.id.get()),
+                    Atomic(op, x, y) => StaticFormKind::Atomic(*op, x.id, y.id),
+                    And(x, y) => StaticFormKind::And(x.id, y.id),
+                    Or(x, y) => StaticFormKind::Or(x.id, y.id),
                     Not(_) | Uninit => panic!(),
                 },
             });
@@ -708,12 +700,12 @@ impl<'a> Visit<'a> for FindMaximalTerms {
         match t.vars {
             VarSet::X => {
                 if !matches!(t.kind, TermKind::Var(_)) {
-                    self.mx.push(t.id.get());
+                    self.mx.push(t.id);
                 }
             }
             VarSet::Y => {
                 if !matches!(t.kind, TermKind::Var(_)) {
-                    self.my.push(t.id.get());
+                    self.my.push(t.id);
                 }
             }
             VarSet::XY => traverse_term(self, t),
@@ -829,16 +821,16 @@ mod tests {
 
     #[test]
     fn normalize_forms() {
-        test_normalize_forms("!(x = y)", "(Neq x y)");
-        test_normalize_forms("!(x <= y)", "(Nle x y)");
-        test_normalize_forms("!(x < y)", "(Nlt x y)");
-        test_normalize_forms("!(x >= y)", "(Nge x y)");
-        test_normalize_forms("!(x > y)", "(Ngt x y)");
-        test_normalize_forms("!!(x = y)", "(Eq x y)");
-        test_normalize_forms("!!(x <= y)", "(Le x y)");
-        test_normalize_forms("!!(x < y)", "(Lt x y)");
-        test_normalize_forms("!!(x >= y)", "(Ge x y)");
-        test_normalize_forms("!!(x > y)", "(Gt x y)");
+        test_normalize_forms("!x = y", "(Neq x y)");
+        test_normalize_forms("!x <= y", "(Nle x y)");
+        test_normalize_forms("!x < y", "(Nlt x y)");
+        test_normalize_forms("!x >= y", "(Nge x y)");
+        test_normalize_forms("!x > y", "(Ngt x y)");
+        test_normalize_forms("!!x = y", "(Eq x y)");
+        test_normalize_forms("!!x <= y", "(Le x y)");
+        test_normalize_forms("!!x < y", "(Lt x y)");
+        test_normalize_forms("!!x >= y", "(Ge x y)");
+        test_normalize_forms("!!x > y", "(Gt x y)");
         test_normalize_forms("!(x = y && y = z)", "(Or (Not (Eq x y)) (Not (Eq y z)))");
         test_normalize_forms("!(x = y || y = z)", "(And (Not (Eq x y)) (Not (Eq y z)))");
     }

src/ast.rs
@@ -1,17 +1,16 @@
-use crate::interval_set::{Site, TupperIntervalSet};
+use crate::interval_set::TupperIntervalSet;
 use bitflags::*;
 use std::{
-    cell::Cell,
     collections::hash_map::DefaultHasher,
     fmt,
     hash::{Hash, Hasher},
 };
 
 pub type TermId = u32;
-const UNINIT_TERM_ID: TermId = TermId::MAX;
+pub const UNINIT_TERM_ID: TermId = TermId::MAX;
 
 pub type FormId = u32;
-const UNINIT_FORM_ID: FormId = FormId::MAX;
+pub const UNINIT_FORM_ID: FormId = FormId::MAX;
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub enum UnaryOp {
@@ -127,8 +126,7 @@ bitflags! {
 /// An AST node for a term.
 #[derive(Clone, Debug)]
 pub struct Term {
-    pub id: Cell<TermId>,
-    pub site: Cell<Option<Site>>,
+    pub id: TermId,
     pub kind: TermKind,
     /// The set of the free variables in the term.
     pub vars: VarSet,
@@ -138,8 +136,7 @@ pub struct Term {
 impl Term {
     pub fn new(kind: TermKind) -> Self {
         Self {
-            id: Cell::new(UNINIT_TERM_ID),
-            site: Cell::new(None),
+            id: UNINIT_TERM_ID,
             kind,
             vars: VarSet::EMPTY,
             internal_hash: 0,
@@ -261,8 +258,7 @@ impl Term {
 impl Default for Term {
     fn default() -> Self {
         Self {
-            id: Cell::new(UNINIT_TERM_ID),
-            site: Cell::new(None),
+            id: UNINIT_TERM_ID,
             kind: TermKind::Uninit,
             vars: VarSet::EMPTY,
             internal_hash: 0,
@@ -326,7 +322,7 @@ pub enum FormKind {
 /// An AST node for a formula.
 #[derive(Clone, Debug)]
 pub struct Form {
-    pub id: Cell<FormId>,
+    pub id: FormId,
     pub kind: FormKind,
     internal_hash: u64,
 }
@@ -334,7 +330,7 @@ pub struct Form {
 impl Form {
     pub fn new(kind: FormKind) -> Self {
         Self {
-            id: Cell::new(UNINIT_FORM_ID),
+            id: UNINIT_FORM_ID,
             kind,
             internal_hash: 0,
         }
@@ -360,7 +356,7 @@ impl Form {
 impl Default for Form {
     fn default() -> Self {
         Self {
-            id: Cell::new(UNINIT_FORM_ID),
+            id: UNINIT_FORM_ID,
             kind: FormKind::Uninit,
             internal_hash: 0,
         }

src/dyn_relation.rs
@@ -318,9 +318,9 @@ impl FromStr for DynRelation {
         }
         UpdateMetadata.visit_form_mut(&mut form);
         let mut v = AssignIdStage1::new();
-        v.visit_form(&form);
+        v.visit_form_mut(&mut form);
         let mut v = AssignIdStage2::new(v);
-        v.visit_form(&form);
+        v.visit_form_mut(&mut form);
         let mut v = CollectStatic::new(v);
         v.visit_form(&form);
         let (terms, forms) = v.terms_forms();

commit dd2089c03dd94c9268a2b0b2b238c623fb230d05

Fix id assignment bug introduced by 4c863d510e37f5150214180415cdb083a08e88d0

rust-toolchain
@@ -1,1 +1,1 @@
-nightly
+nightly-2021-02-25

src/visit.rs
@@ -9,8 +9,10 @@ use crate::{
 use inari::Decoration;
 use std::{
     collections::{HashMap, HashSet},
+    hash::{Hash, Hasher},
     marker::Sized,
     mem::{swap, take},
+    ops::Deref,
 };
 
 /// A type that traverses formulas and terms in depth-first order.
@@ -112,6 +114,40 @@ fn traverse_form_mut<V: VisitMut>(v: &mut V, f: &mut Form) {
         }
         Uninit => (),
     };
+}
+
+/// A possibly dangling reference to a value.
+/// All operations except [`UnsafeRef<T>::from`] are unsafe.
+struct UnsafeRef<T: Eq + Hash> {
+    ptr: *const T,
+}
+
+impl<T: Eq + Hash> UnsafeRef<T> {
+    fn from(t: &T) -> Self {
+        Self { ptr: t as *const T }
+    }
+}
+
+impl<T: Eq + Hash> Deref for UnsafeRef<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.ptr }
+    }
+}
+
+impl<T: Eq + Hash> PartialEq for UnsafeRef<T> {
+    fn eq(&self, rhs: &Self) -> bool {
+        unsafe { (*self.ptr) == (*rhs.ptr) }
+    }
+}
+
+impl<T: Eq + Hash> Eq for UnsafeRef<T> {}
+
+impl<T: Eq + Hash> Hash for UnsafeRef<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        unsafe { (*self.ptr).hash(state) }
+    }
 }
 
 pub struct Substitute {
@@ -458,6 +494,8 @@ impl VisitMut for UpdateMetadata {
 }
 
 type SiteMap = HashMap<TermId, Site>;
+type UnsafeTermRef = UnsafeRef<Term>;
+type UnsafeFormRef = UnsafeRef<Form>;
 
 /// Assigns [`TermId`]s to the scalar terms and [`FormId`]s to the atomic formulas.
 /// These [`FormId`]s are used as indices in [`EvalResult`][`crate::eval_result::EvalResult`].
@@ -466,8 +504,8 @@ pub struct AssignIdStage1 {
     next_term_id: TermId,
     next_site: u8,
     site_map: SiteMap,
-    visited_forms: HashSet<*const Form>,
-    visited_terms: HashSet<*const Term>,
+    visited_forms: HashSet<UnsafeFormRef>,
+    visited_terms: HashSet<UnsafeTermRef>,
 }
 
 impl AssignIdStage1 {
@@ -513,9 +551,9 @@ impl VisitMut for AssignIdStage1 {
         traverse_term_mut(self, t);
 
         if !matches!(t.kind, TermKind::List(_)) {
-            match self.visited_terms.get(&(t as *const Term)) {
+            match self.visited_terms.get(&UnsafeTermRef::from(t)) {
                 Some(visited) => {
-                    let id = unsafe { (**visited).id };
+                    let id = visited.id;
                     t.id = id;
 
                     if !self.site_map.contains_key(&id)
@@ -530,7 +568,7 @@ impl VisitMut for AssignIdStage1 {
                     assert!(self.next_term_id != UNINIT_TERM_ID);
                     t.id = self.next_term_id;
                     self.next_term_id += 1;
-                    self.visited_terms.insert(t);
+                    self.visited_terms.insert(UnsafeTermRef::from(t));
                 }
             }
         }
@@ -540,15 +578,15 @@ impl VisitMut for AssignIdStage1 {
         traverse_form_mut(self, f);
 
         if let FormKind::Atomic(_, _, _) = f.kind {
-            match self.visited_forms.get(&(f as *const Form)) {
+            match self.visited_forms.get(&UnsafeFormRef::from(f)) {
                 Some(visited) => {
-                    f.id = unsafe { (**visited).id };
+                    f.id = visited.id;
                 }
                 _ => {
                     assert!(self.next_form_id != UNINIT_FORM_ID);
                     f.id = self.next_form_id;
                     self.next_form_id += 1;
-                    self.visited_forms.insert(f);
+                    self.visited_forms.insert(UnsafeFormRef::from(f));
                 }
             }
         }
@@ -560,8 +598,8 @@ pub struct AssignIdStage2 {
     next_form_id: FormId,
     next_term_id: TermId,
     site_map: SiteMap,
-    visited_forms: HashSet<*const Form>,
-    visited_terms: HashSet<*const Term>,
+    visited_forms: HashSet<UnsafeFormRef>,
+    visited_terms: HashSet<UnsafeTermRef>,
 }
 
 impl AssignIdStage2 {
@@ -581,15 +619,15 @@ impl VisitMut for AssignIdStage2 {
         traverse_term_mut(self, t);
 
         if let TermKind::List(_) = t.kind {
-            match self.visited_terms.get(&(t as *const Term)) {
+            match self.visited_terms.get(&UnsafeTermRef::from(t)) {
                 Some(visited) => {
-                    t.id = unsafe { (**visited).id };
+                    t.id = visited.id;
                 }
                 _ => {
                     assert!(self.next_term_id != UNINIT_TERM_ID);
                     t.id = self.next_term_id;
                     self.next_term_id += 1;
-                    self.visited_terms.insert(t);
+                    self.visited_terms.insert(UnsafeTermRef::from(t));
                 }
             }
         }
@@ -599,15 +637,15 @@ impl VisitMut for AssignIdStage2 {
         traverse_form_mut(self, f);
 
         if !matches!(f.kind, FormKind::Atomic(_, _, _)) {
-            match self.visited_forms.get(&(f as *const Form)) {
+            match self.visited_forms.get(&UnsafeFormRef::from(f)) {
                 Some(visited) => {
-                    f.id = unsafe { (**visited).id };
+                    f.id = visited.id;
                 }
                 _ => {
                     assert!(self.next_form_id != UNINIT_FORM_ID);
                     f.id = self.next_form_id;
                     self.next_form_id += 1;
-                    self.visited_forms.insert(f);
+                    self.visited_forms.insert(UnsafeFormRef::from(f));
                 }
             }
         }