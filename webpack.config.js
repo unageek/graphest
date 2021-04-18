80aded383cecd86b4d760000d959f86d573653e8
Masashi Mizuno <29035331+unageek@users.noreply.github.com>
2021-04-18 20:03:21 +0900

    Organize scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,23 @@ const path = require("path");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const baseConfig = {
-  devtool: "cheap-source-map",
-  mode: "development",
-  stats: "errors-warnings",
-};
+function baseConfig(mode) {
+  switch (mode) {
+    case "development":
+      return {
+        devtool: "cheap-source-map",
+        mode: "development",
+        stats: "errors-warnings",
+      };
+    case "production":
+      return {
+        mode: "production",
+        stats: "errors-warnings",
+      };
+    default:
+      throw "mode must be either 'development' or 'production'";
+  }
+}
 
 const tsLoaderRule = {
   include: path.resolve(__dirname, "src"),
@@ -22,69 +34,79 @@ const esLintPlugin = new ESLintPlugin({
   fix: true,
 });
 
-const mainConfig = {
-  ...baseConfig,
-  target: "electron-main",
-  entry: "./src/main.ts",
-  module: {
-    rules: [tsLoaderRule],
-  },
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "main.js",
-  },
-  plugins: [esLintPlugin],
-};
+function mainConfig(mode) {
+  return {
+    ...baseConfig(mode),
+    target: "electron-main",
+    entry: "./src/main.ts",
+    module: {
+      rules: [tsLoaderRule],
+    },
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "main.js",
+    },
+    plugins: [esLintPlugin],
+  };
+}
 
-const preloadConfig = {
-  ...baseConfig,
-  target: "electron-preload",
-  entry: "./src/preload.ts",
-  module: {
-    rules: [tsLoaderRule],
-  },
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "preload.js",
-  },
-  plugins: [esLintPlugin],
-};
+function preloadConfig(mode) {
+  return {
+    ...baseConfig(mode),
+    target: "electron-preload",
+    entry: "./src/preload.ts",
+    module: {
+      rules: [tsLoaderRule],
+    },
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "preload.js",
+    },
+    plugins: [esLintPlugin],
+  };
+}
 
-const rendererConfig = {
-  ...baseConfig,
-  target: "electron-renderer",
-  entry: {
-    bundle: "./src/App.tsx",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(eot|png|svg|ttf|woff|woff2)$/,
-        type: "asset/resource",
-      },
-      {
-        ...tsLoaderRule,
-        test: /\.tsx?$/,
-        resolve: {
-          extensions: [".ts", ".tsx", ".js"],
+function rendererConfig(mode) {
+  return {
+    ...baseConfig(mode),
+    target: "electron-renderer",
+    entry: {
+      bundle: "./src/App.tsx",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
         },
-      },
+        {
+          test: /\.(eot|png|svg|ttf|woff|woff2)$/,
+          type: "asset/resource",
+        },
+        {
+          ...tsLoaderRule,
+          test: /\.tsx?$/,
+          resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+          },
+        },
+      ],
+    },
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "[name].js",
+    },
+    plugins: [
+      esLintPlugin,
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].js",
-  },
-  plugins: [
-    esLintPlugin,
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-};
+  };
+}
 
-module.exports = [mainConfig, preloadConfig, rendererConfig];
+module.exports = (env, argv) => {
+  return [mainConfig, preloadConfig, rendererConfig].map((cfg) =>
+    cfg(argv.mode)
+  );
+};
