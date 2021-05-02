83f760d902bd69db64f2481fc228d0637e30200a
Masashi Mizuno <29035331+unageek@users.noreply.github.com>
2021-05-02 22:43:43 +0900

    Use PrefixVarint to encode small integers

--- rust/src/image_block_queue.rs
+++ rust/src/image_block_queue.rs
@@ -1,5 +1,5 @@
 use crate::graph::ImageBlock;
-use std::{collections::VecDeque, mem::size_of};
+use std::{collections::VecDeque, mem::size_of, ptr::copy_nonoverlapping};
 
 /// A queue for storing [`ImageBlock`]s.
 /// The closer the indices of consecutive blocks are (which is expected by using the Morton order),
@@ -59,35 +59,68 @@ impl ImageBlockQueue {
         Some(self.seq.pop_front()? as i8)
     }
 
+    // PrefixVarint[0][1] is used to encode unsigned numbers:
+    //
+    //    Range   `zeros`  Encoded bytes in `seq`
+    //   ------  --------  ----------------------------------------------------------------------
+    //                        6     0 -- Bit place in the original number
+    //    < 2^7         0  [0bxxxxxxx1]
+    //                        5    0     13      6
+    //   < 2^14         1  [0bxxxxxx10, 0byyyyyyyy]
+    //                        4   0      12      5   20     13
+    //   < 2^21         2  [0bxxxxx100, 0byyyyyyyy, 0byyyyyyyy]
+    //                        3  0       11      4   19     12   27     20
+    //   < 2^28         3  [0bxxxx1000, 0byyyyyyyy, 0byyyyyyyy, 0byyyyyyyy]
+    //                        2 0        10      3   18     11   26     19      31  27
+    //   < 2^32         4  [0bxxx10000, 0byyyyyyyy, 0byyyyyyyy, 0byyyyyyyy, 0b000yyyyy]
+    //                  |               -----------------------v----------------------
+    //                  |               These bytes can be interpreted as a part of a `u32` value
+    //                  |               in little endian.
+    //                  The number of trailing zeros in the first byte.
+    //
+    // [0]: https://github.com/stoklund/varint#prefixvarint
+    // [1]: https://news.ycombinator.com/item?id=11263667
     fn pop_small_u32(&mut self) -> Option<u32> {
-        let mut byte = self.seq.pop_front()?;
-        let mut shift = 0;
-        let mut x = (byte & 0x7f) as u32;
-        while byte & 0x80 != 0 {
-            byte = self.seq.pop_front()?;
-            shift += 7;
-            x |= ((byte & 0x7f) as u32) << shift;
-        }
-        Some(x)
+        let head = self.seq.pop_front()?;
+        let zeros = head.trailing_zeros();
+        let tail_len = zeros as usize;
+        let (tail1, tail2) = {
+            let (mut t1, mut t2) = self.seq.as_slices();
+            t1 = &t1[..tail_len.min(t1.len())];
+            t2 = &t2[..(tail_len - t1.len())];
+            (t1, t2)
+        };
+        let x = (head >> (zeros + 1)) as u32;
+        let y = {
+            let mut y = 0u32;
+            let y_ptr = &mut y as *mut u32 as *mut u8;
+            unsafe {
+                copy_nonoverlapping(tail1.as_ptr(), y_ptr, tail1.len());
+                copy_nonoverlapping(tail2.as_ptr(), y_ptr.add(tail1.len()), tail2.len());
+            }
+            y = u32::from_le(y);
+            y << (7 - zeros)
+        };
+        self.seq.drain(..tail_len);
+        Some(x | y)
     }
 
     fn push_i8(&mut self, x: i8) {
         self.seq.push_back(x as u8);
     }
 
-    // The value is divided into 7-bit chunks and only the least significant chunks are stored.
-    // For example, a 16-bit value is stored as:
-    //
-    //   0b00_0000000_00000xx → [0b0_00000xx]
-    //   0b00_0000000_xxxxxxx → [0b0_xxxxxxx]
-    //   0b00_yyyyyyy_xxxxxxx → [0b1_xxxxxxx, 0b0_yyyyyyy]
-    //   0bzz_yyyyyyy_xxxxxxx → [0b1_xxxxxxx, 0b1_yyyyyyy, 0b0_00000zz]
-    fn push_small_u32(&mut self, mut x: u32) {
-        while x >= 0x80 {
-            self.seq.push_back(0x80 | (x & 0x7f) as u8);
-            x >>= 7;
-        }
-        self.seq.push_back(x as u8);
+    fn push_small_u32(&mut self, x: u32) {
+        let zeros = match x {
+            0..=0x7f => 0,
+            0x80..=0x3fff => 1,
+            0x4000..=0x1fffff => 2,
+            0x200000..=0xfffffff => 3,
+            0x10000000..=0xFFFFFFFF => 4,
+        };
+        self.seq.push_back((((x << 1) | 0x1) << zeros) as u8);
+        let y = x >> (7 - zeros);
+        let tail_len = zeros;
+        self.seq.extend(y.to_le_bytes()[..tail_len].iter());
     }
 }
 
@@ -99,6 +132,12 @@ mod tests {
     fn push_pop() {
         let mut queue = ImageBlockQueue::new();
         let blocks = [
+            ImageBlock {
+                x: 0,
+                y: 0xFFFFFFFF,
+                kx: -128,
+                ky: 127,
+            },
             ImageBlock {
                 x: 0x7F,
                 y: 0x10000000,
@@ -146,6 +185,12 @@ mod tests {
                 y: 0x7F,
                 kx: 127,
                 ky: -128,
+            },
+            ImageBlock {
+                x: 0xFFFFFFFF,
+                y: 0,
+                kx: -128,
+                ky: 127,
             },
         ];
         for b in blocks.iter().copied() {
