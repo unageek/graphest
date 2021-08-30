d11be56b411aaa00f0fa94d6fd486a0eb468a25f
unageek <29035331+unageek@users.noreply.github.com>
2021-08-30 22:32:56 +0900

    Cache C headers and libraries

--- rust/build.rs
+++ rust/build.rs
@@ -1,11 +1,25 @@
-use std::{env::var_os, ffi::OsString, fs::create_dir_all, path::PathBuf, process::Command};
+use std::{
+    env,
+    ffi::OsString,
+    fs, io,
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 // https://gitlab.com/tspiteri/gmp-mpfr-sys/-/blob/master/build.rs
 
+// Should be bumped whenever any change is made to this file.
+const VERSION: &str = "0.1.0";
+
+const ARB_GIT_TAG: &str = "2.20.0";
+const ARB_GIT_URL: &str = "https://github.com/fredrik-johansson/arb.git";
+
+const FLINT_GIT_TAG: &str = "v2.8.0";
+const FLINT_GIT_URL: &str = "https://github.com/wbhart/flint2.git";
+
 struct Environment {
-    arb_branch: OsString,
     build_dir: PathBuf,
-    flint_branch: OsString,
+    cache_dir: Option<PathBuf>,
     gmp_dir: PathBuf,
     include_dir: PathBuf,
     lib_dir: PathBuf,
@@ -14,32 +28,37 @@ struct Environment {
 }
 
 fn main() {
-    if var_os("CARGO_FEATURE_ARB").is_none() {
+    if env::var_os("CARGO_FEATURE_ARB").is_none() {
         return;
     }
 
-    let out_dir = PathBuf::from(var_os("OUT_DIR").unwrap());
+    let cache_dir = user_cache_dir().map(|c| c.join("graphest-arb-sys").join(VERSION));
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     let env = Environment {
-        arb_branch: var_os("ARB_BRANCH").unwrap_or_else(|| "master".into()),
         build_dir: out_dir.join("build"),
-        flint_branch: var_os("FLINT_BRANCH").unwrap_or_else(|| "trunk".into()),
-        gmp_dir: PathBuf::from(var_os("DEP_GMP_OUT_DIR").unwrap()),
+        cache_dir: cache_dir,
+        gmp_dir: PathBuf::from(env::var_os("DEP_GMP_OUT_DIR").unwrap()),
         include_dir: out_dir.join("include"),
         lib_dir: out_dir.join("lib"),
-        makeflags: var_os("CARGO_MAKEFLAGS").unwrap(),
+        makeflags: env::var_os("CARGO_MAKEFLAGS").unwrap(),
         out_dir: out_dir.clone(),
     };
-    create_dir_all(&env.build_dir).unwrap_or_else(|_| {
-        panic!(
-            "failed to create a directory: {}",
-            env.build_dir.to_string_lossy()
-        )
-    });
+    fs::create_dir_all(&env.build_dir)
+        .unwrap_or_else(|_| panic!("failed to create the directory: {:?}", env.build_dir));
 
+    load_cache(&env);
     build_flint(&env);
     build_arb(&env);
+    save_cache(&env);
     run_bindgen(&env);
     write_link_info(&env);
+}
+
+fn load_cache(env: &Environment) {
+    if let Some(c) = &env.cache_dir {
+        let _ = copy_dir_all(c.join("include"), env.include_dir.clone());
+        let _ = copy_dir_all(c.join("lib"), env.lib_dir.clone());
+    }
 }
 
 fn build_flint(env: &Environment) {
@@ -48,19 +67,18 @@ fn build_flint(env: &Environment) {
     }
 
     let build_dir = env.build_dir.join("flint-build");
-
     if !build_dir.exists() {
-        execute(Command::new("git").current_dir(&env.build_dir).args(&[
+        execute_or_panic(Command::new("git").current_dir(&env.build_dir).args(&[
             "clone",
             "--branch",
-            env.flint_branch.to_str().unwrap(),
+            FLINT_GIT_TAG,
             "--depth",
             "1",
-            "https://github.com/fredrik-johansson/flint2.git",
+            FLINT_GIT_URL,
             build_dir.to_str().unwrap(),
         ]));
     }
-    execute(
+    execute_or_panic(
         Command::new(build_dir.join("configure"))
             .current_dir(&build_dir)
             .args([
@@ -71,18 +89,18 @@ fn build_flint(env: &Environment) {
             ])
             .env("CFLAGS", "-Wno-error"),
     );
-    execute(
+    execute_or_panic(
         Command::new("make")
             .current_dir(&build_dir)
             .env("MAKEFLAGS", &env.makeflags),
     );
-    execute(
+    execute_or_panic(
         Command::new("make")
             .current_dir(&build_dir)
             .arg("check")
             .env("MAKEFLAGS", &env.makeflags),
     );
-    execute(Command::new("make").current_dir(&build_dir).arg("install"));
+    execute_or_panic(Command::new("make").current_dir(&build_dir).arg("install"));
 }
 
 fn build_arb(env: &Environment) {
@@ -92,17 +110,17 @@ fn build_arb(env: &Environment) {
 
     let build_dir = env.build_dir.join("arb-build");
     if !build_dir.exists() {
-        execute(Command::new("git").current_dir(&env.build_dir).args(&[
+        execute_or_panic(Command::new("git").current_dir(&env.build_dir).args(&[
             "clone",
             "--branch",
-            env.arb_branch.to_str().unwrap(),
+            ARB_GIT_TAG,
             "--depth",
             "1",
-            "https://github.com/fredrik-johansson/arb.git",
+            ARB_GIT_URL,
             build_dir.to_str().unwrap(),
         ]));
     }
-    execute(
+    execute_or_panic(
         Command::new(build_dir.join("configure"))
             .current_dir(&build_dir)
             .args([
@@ -114,19 +132,19 @@ fn build_arb(env: &Environment) {
             ])
             .env("CFLAGS", "-Wno-error"),
     );
-    execute(
+    execute_or_panic(
         Command::new("make")
             .current_dir(&build_dir)
             .env("MAKEFLAGS", &env.makeflags),
     );
-    execute(
+    execute_or_panic(
         Command::new("make")
             .current_dir(&build_dir)
             .arg("check")
             .env("ARB_TEST_MULTIPLIER", "0.1")
             .env("MAKEFLAGS", &env.makeflags),
     );
-    execute(Command::new("make").current_dir(&build_dir).arg("install"));
+    execute_or_panic(Command::new("make").current_dir(&build_dir).arg("install"));
 }
 
 fn run_bindgen(env: &Environment) {
@@ -134,6 +152,7 @@ fn run_bindgen(env: &Environment) {
     if binding_file.exists() {
         return;
     }
+
     bindgen::Builder::default()
         .header(env.include_dir.join("acb.h").to_str().unwrap())
         .header(env.include_dir.join("acb_elliptic.h").to_str().unwrap())
@@ -155,10 +174,17 @@ fn run_bindgen(env: &Environment) {
         .write_to_file(binding_file.clone())
         .unwrap_or_else(|_| {
             panic!(
-                "failed to write to a file: {}",
+                "failed to write to the file: {}",
                 binding_file.to_string_lossy()
             )
         });
+}
+
+fn save_cache(env: &Environment) {
+    if let Some(c) = &env.cache_dir {
+        let _ = copy_dir_all(env.include_dir.clone(), c.join("include"));
+        let _ = copy_dir_all(env.lib_dir.clone(), c.join("lib"));
+    }
 }
 
 fn write_link_info(env: &Environment) {
@@ -176,15 +202,58 @@ fn write_link_info(env: &Environment) {
     println!("cargo:rustc-link-lib=static=arb");
 }
 
-fn execute(cmd: &mut Command) {
+/// Copies all files and directories in `from` into `to`, preserving the directory structure.
+///
+/// The directory `to` is created if it does not exist. Symlinks are ignored.
+fn copy_dir_all<P: AsRef<Path>, Q: AsRef<Path>>(from: P, to: Q) -> io::Result<()> {
+    fs::create_dir_all(&to)?;
+
+    for entry in fs::read_dir(from)? {
+        let entry = entry?;
+        let from = entry.path();
+        let to = to.as_ref().join(entry.file_name());
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            copy_dir_all(from, to)?;
+        } else if ty.is_file() {
+            fs::copy(from, to)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn execute_or_panic(cmd: &mut Command) {
     let status = cmd
         .status()
-        .unwrap_or_else(|_| panic!("failed to run the command: {:?}", cmd));
+        .unwrap_or_else(|_| panic!("failed to execute the command: {:?}", cmd));
+
     if !status.success() {
         if let Some(code) = status.code() {
-            panic!("command exited with code {}: {:?}", code, cmd);
+            panic!("the process exited with code {}: {:?}", code, cmd);
         } else {
-            panic!("command failed: {:?}", cmd);
+            panic!("the process is terminated by a signal: {:?}", cmd);
         }
+    }
+}
+
+fn user_cache_dir() -> Option<PathBuf> {
+    let host = env::var("HOST").ok()?;
+
+    if host.contains("darwin") {
+        env::var_os("HOME")
+            .filter(|s| !s.is_empty())
+            .map(|s| PathBuf::from(s).join("Library").join("Caches"))
+    } else if host.contains("linux") {
+        env::var_os("XDG_CACHE_HOME")
+            .filter(|s| !s.is_empty())
+            .map(PathBuf::from)
+            .or_else(|| {
+                env::var_os("HOME")
+                    .filter(|s| !s.is_empty())
+                    .map(|s| PathBuf::from(s).join(".cache"))
+            })
+    } else {
+        None
     }
 }
