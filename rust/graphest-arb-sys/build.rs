184be93a05d5f9bfc116a3ab0777a10fe8583eb7
unageek <29035331+unageek@users.noreply.github.com>
2021-10-10 17:04:56 +0900

    Update FLINT to v2.8.2 and Arb to v2.21.1

--- rust/graphest-arb-sys/build.rs
+++ rust/graphest-arb-sys/build.rs
@@ -6,10 +6,10 @@ use std::{
 
 // https://gitlab.com/tspiteri/gmp-mpfr-sys/-/blob/master/build.rs
 
-const ARB_GIT_TAG: &str = "2.20.0";
+const ARB_GIT_TAG: &str = "2.21.1";
 const ARB_GIT_URL: &str = "https://github.com/fredrik-johansson/arb.git";
 
-const FLINT_GIT_TAG: &str = "v2.8.0";
+const FLINT_GIT_TAG: &str = "v2.8.2";
 const FLINT_GIT_URL: &str = "https://github.com/wbhart/flint2.git";
 
 struct Environment {
