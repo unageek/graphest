unageek/graphest

commit 6e76a7eb963733dd1a9b26d094c5bde78d824491

Refactor subdivision direction selection

rust/src/block.rs
@@ -264,7 +264,6 @@ pub struct Block {
     /// The parameter t.
     pub t: RealParameter,
     /// The component(s) of the block that should be subdivided next.
-    // TODO: It is awkward that blocks have this field.
     pub next_dir: VarSet,
 }
 

rust/src/graph/implicit.rs
@@ -17,6 +17,7 @@ use itertools::Itertools;
 use smallvec::smallvec;
 use std::{
     convert::TryFrom,
+    iter::once,
     time::{Duration, Instant},
 };
 
@@ -27,6 +28,7 @@ const XY: VarSet = VarSet::from_bits_truncate(VarSet::X.bits() | VarSet::Y.bits(
 pub struct Implicit {
     rel: Relation,
     vars: VarSet,
+    dirs: Vec<VarSet>,
     im: Image<PixelState>,
     // Queue blocks that will be subdivided instead of the divided blocks to save memory.
     bs_to_subdivide: BlockQueue,
@@ -50,9 +52,15 @@ impl Implicit {
         assert_eq!(rel.relation_type(), RelationType::Implicit);
 
         let vars = rel.vars();
+        let dirs = [XY, VarSet::N_THETA, VarSet::T]
+            .into_iter()
+            .filter(|&d| (XY | vars).contains(d))
+            .collect();
+
         let mut g = Self {
             rel,
             vars,
+            dirs,
             im: Image::new(im_width, im_height),
             bs_to_subdivide: BlockQueue::new(XY | vars),
             im_to_real: Transform2D::new(
@@ -92,7 +100,7 @@ impl Implicit {
             x: Coordinate::new(0, k),
             y: Coordinate::new(0, k),
             t: RealParameter::new(g.rel.t_range()),
-            next_dir: XY,
+            next_dir: g.dirs[0],
             ..Block::default()
         }];
 
@@ -170,37 +178,31 @@ impl Implicit {
                 b.next_dir
             } else {
                 // Subdivide in other direction.
-                match b.next_dir {
-                    XY if self.vars.contains(VarSet::N_THETA) => VarSet::N_THETA,
-                    XY if self.vars.contains(VarSet::T) => VarSet::T,
-                    XY => XY,
-                    VarSet::N_THETA if self.vars.contains(VarSet::T) => VarSet::T,
-                    VarSet::N_THETA => XY,
-                    VarSet::T => XY,
-                    _ => panic!(),
-                }
+                let mut it = self.dirs.iter().copied().cycle();
+                it.find(|&d| d == b.next_dir);
+                it.next().unwrap()
             };
 
             for mut sub_b in incomplete_sub_bs.drain(..) {
-                sub_b.next_dir = if preferred_next_dir == VarSet::N_THETA
-                    && sub_b.n_theta.is_subdivisible()
-                    || preferred_next_dir == VarSet::T && sub_b.t.is_subdivisible()
-                {
-                    preferred_next_dir
-                } else if sub_b.x.is_subdivisible() {
-                    XY
-                } else if self.vars.contains(VarSet::N_THETA) && sub_b.n_theta.is_subdivisible() {
-                    VarSet::N_THETA
-                } else if self.vars.contains(VarSet::T) && sub_b.t.is_subdivisible() {
-                    VarSet::T
-                } else {
-                    // Cannot subdivide in any direction.
-                    assert!(sub_b.x.is_subpixel());
-                    for p in &self.pixels_in_image(&b) {
-                        self.im[p] = PixelState::Uncertain(None);
+                let next_dir = once(preferred_next_dir)
+                    .chain(self.dirs.iter().copied())
+                    .find(|&d| {
+                        d == XY && sub_b.x.is_subdivisible()
+                            || d == VarSet::N_THETA && sub_b.n_theta.is_subdivisible()
+                            || d == VarSet::T && sub_b.t.is_subdivisible()
+                    });
+
+                match next_dir {
+                    Some(d) => sub_b.next_dir = d,
+                    _ => {
+                        // Cannot subdivide in any direction.
+                        assert!(sub_b.x.is_subpixel());
+                        for p in &self.pixels_in_image(&b) {
+                            self.im[p] = PixelState::Uncertain(None);
+                        }
+                        continue;
                     }
-                    continue;
-                };
+                }
 
                 self.bs_to_subdivide.push_back(sub_b.clone());
                 let last_bi = self.bs_to_subdivide.end_index() - 1;

commit 1c9215c1f147515c994c208eb0524e5ecbd90591

Fix subdivision direction selection broken by 6e76a7eb963733dd1a9b26d094c5bde78d824491

rust/src/block.rs
@@ -290,8 +290,9 @@ pub struct Block {
     pub n_theta: IntegerParameter,
     /// The parameter t.
     pub t: RealParameter,
-    /// The component(s) of the block that should be subdivided next.
-    pub next_dir: VarSet,
+    /// The index of the subdivision direction, defined by the graphing algorithm,
+    /// that should be chosen to subdivide this block.
+    pub next_dir_index: u8,
 }
 
 const DEFAULT_BLOCK: Block = Block {
@@ -301,7 +302,7 @@ const DEFAULT_BLOCK: Block = Block {
     n: IntegerParameter(Interval::ENTIRE),
     n_theta: IntegerParameter(Interval::ENTIRE),
     t: RealParameter(Interval::ENTIRE),
-    next_dir: VarSet::EMPTY,
+    next_dir_index: 0,
 };
 
 /// A queue that stores [`Block`]s.
@@ -420,7 +421,7 @@ impl BlockQueue {
             b.t = RealParameter(self.t_front);
         }
 
-        b.next_dir = self.pop_vars()?;
+        b.next_dir_index = self.pop_u8()?;
 
         self.begin_index += 1;
 
@@ -465,7 +466,7 @@ impl BlockQueue {
             self.t_back = t;
         }
 
-        self.push_vars(b.next_dir);
+        self.push_u8(b.next_dir_index);
 
         self.end_index += 1;
     }
@@ -546,10 +547,8 @@ impl BlockQueue {
         Some(x | y)
     }
 
-    fn pop_vars(&mut self) -> Option<VarSet> {
-        let bits = self.seq.pop_front()?;
-        let vars = unsafe { VarSet::from_bits_unchecked(bits) };
-        Some(vars)
+    fn pop_u8(&mut self) -> Option<u8> {
+        self.seq.pop_front()
     }
 
     fn push_i8(&mut self, x: i8) {
@@ -584,8 +583,8 @@ impl BlockQueue {
         self.seq.extend(y.to_le_bytes()[..tail_len].iter());
     }
 
-    fn push_vars(&mut self, vars: VarSet) {
-        self.seq.push_back(vars.bits());
+    fn push_u8(&mut self, x: u8) {
+        self.seq.push_back(x)
     }
 }
 

rust/src/graph/implicit.rs
@@ -21,7 +21,6 @@ use smallvec::smallvec;
 use std::{
     convert::TryFrom,
     default::default,
-    iter::once,
     time::{Duration, Instant},
 };
 
@@ -104,7 +103,6 @@ impl Implicit {
             x: Coordinate::new(0, k),
             y: Coordinate::new(0, k),
             t: RealParameter::new(g.rel.t_range()),
-            next_dir: g.subdivision_dirs[0],
             ..default()
         }];
 
@@ -137,11 +135,11 @@ impl Implicit {
     fn refine_impl(&mut self, duration: Duration, now: &Instant) -> Result<bool, GraphingError> {
         let mut sub_bs = vec![];
         let mut incomplete_sub_bs = vec![];
-        let mut next_dir_candidates = vec![];
         let mut args = self.rel.create_args();
         while let Some(b) = self.bs_to_subdivide.pop_front() {
             let bi = self.bs_to_subdivide.begin_index() - 1;
-            match b.next_dir {
+            let next_dir = self.subdivision_dirs[b.next_dir_index as usize];
+            match next_dir {
                 XY => self.subdivide_xy(&mut sub_bs, &b),
                 VarSet::M => subdivide_m(&mut sub_bs, &b),
                 VarSet::N => subdivide_n(&mut sub_bs, &b),
@@ -162,34 +160,30 @@ impl Implicit {
                 }
             }
 
-            let n_max = match b.next_dir {
+            let n_max = match next_dir {
                 XY => 4,
                 VarSet::M | VarSet::N | VarSet::N_THETA => 3,
                 VarSet::T => 1000, // Avoid repeated subdivision of t.
                 _ => panic!(),
             };
-            let next_dir_suggestion = if n_max * incomplete_sub_bs.len() <= n_sub_bs {
-                // Subdivide in the same direction again.
-                b.next_dir
-            } else {
-                // Subdivide in other direction.
-                let mut it = self.subdivision_dirs.iter().copied().cycle();
-                it.find(|&d| d == b.next_dir);
-                it.next().unwrap()
-            };
 
-            next_dir_candidates.splice(
-                ..,
-                once(next_dir_suggestion).chain(
-                    self.subdivision_dirs
-                        .iter()
-                        .copied()
-                        .filter(|&d| d != next_dir_suggestion),
-                ),
-            );
+            let it = (0..self.subdivision_dirs.len())
+                .cycle()
+                .skip(
+                    if n_max * incomplete_sub_bs.len() <= n_sub_bs {
+                        // Subdivide in the same direction again.
+                        b.next_dir_index
+                    } else {
+                        // Subdivide in other direction.
+                        b.next_dir_index + 1
+                    }
+                    .into(),
+                )
+                .take(self.subdivision_dirs.len());
 
             for mut sub_b in incomplete_sub_bs.drain(..) {
-                let next_dir = next_dir_candidates.iter().copied().find(|&d| {
+                let next_dir_index = it.clone().find(|&i| {
+                    let d = self.subdivision_dirs[i];
                     d == XY && sub_b.x.is_subdivisible()
                         || d == VarSet::M && sub_b.m.is_subdivisible()
                         || d == VarSet::N && sub_b.n.is_subdivisible()
@@ -197,8 +191,8 @@ impl Implicit {
                         || d == VarSet::T && sub_b.t.is_subdivisible()
                 });
 
-                if let Some(d) = next_dir {
-                    sub_b.next_dir = d;
+                if let Some(i) = next_dir_index {
+                    sub_b.next_dir_index = i as u8;
                 } else {
                     // Cannot subdivide in any direction.
                     assert!(sub_b.x.is_subpixel());

rust/src/graph/parametric.rs
@@ -20,7 +20,6 @@ use smallvec::SmallVec;
 use std::{
     convert::TryFrom,
     default::default,
-    iter::once,
     mem::swap,
     time::{Duration, Instant},
 };
@@ -102,7 +101,6 @@ impl Parametric {
 
         g.bs_to_subdivide.push_back(Block {
             t: RealParameter::new(g.rel.t_range()),
-            next_dir: g.subdivision_dirs[0],
             ..default()
         });
 
@@ -113,11 +111,11 @@ impl Parametric {
         let mut sub_bs = vec![];
         let mut incomplete_pixels = vec![];
         let mut incomplete_sub_bs = vec![];
-        let mut next_dir_candidates = vec![];
         let mut args = self.rel.create_args();
         while let Some(b) = self.bs_to_subdivide.pop_front() {
             let bi = self.bs_to_subdivide.begin_index() - 1;
-            match b.next_dir {
+            let next_dir = self.subdivision_dirs[b.next_dir_index as usize];
+            match next_dir {
                 VarSet::M => subdivide_m(&mut sub_bs, &b),
                 VarSet::N => subdivide_n(&mut sub_bs, &b),
                 VarSet::T => subdivide_t(&mut sub_bs, &b),
@@ -136,40 +134,36 @@ impl Parametric {
                 incomplete_pixels.clear();
             }
 
-            let n_max = match b.next_dir {
+            let n_max = match next_dir {
                 VarSet::M | VarSet::N => 3,
                 VarSet::T => 1000, // Avoid repeated subdivision of t.
                 _ => panic!(),
             };
-            let next_dir_suggestion = if n_max * incomplete_sub_bs.len() <= n_sub_bs {
-                // Subdivide in the same direction again.
-                b.next_dir
-            } else {
-                // Subdivide in other direction.
-                let mut it = self.subdivision_dirs.iter().copied().cycle();
-                it.find(|&d| d == b.next_dir);
-                it.next().unwrap()
-            };
 
-            next_dir_candidates.splice(
-                ..,
-                once(next_dir_suggestion).chain(
-                    self.subdivision_dirs
-                        .iter()
-                        .copied()
-                        .filter(|&d| d != next_dir_suggestion),
-                ),
-            );
+            let it = (0..self.subdivision_dirs.len())
+                .cycle()
+                .skip(
+                    if n_max * incomplete_sub_bs.len() <= n_sub_bs {
+                        // Subdivide in the same direction again.
+                        b.next_dir_index
+                    } else {
+                        // Subdivide in other direction.
+                        b.next_dir_index + 1
+                    }
+                    .into(),
+                )
+                .take(self.subdivision_dirs.len());
 
             for (mut sub_b, incomplete_pixels) in incomplete_sub_bs.drain(..) {
-                let next_dir = next_dir_candidates.iter().copied().find(|&d| {
+                let next_dir_index = it.clone().find(|&i| {
+                    let d = self.subdivision_dirs[i];
                     d == VarSet::M && sub_b.m.is_subdivisible()
                         || d == VarSet::N && sub_b.n.is_subdivisible()
                         || d == VarSet::T && sub_b.t.is_subdivisible()
                 });
 
-                if let Some(d) = next_dir {
-                    sub_b.next_dir = d;
+                if let Some(i) = next_dir_index {
+                    sub_b.next_dir_index = i as u8;
                 } else {
                     // Cannot subdivide in any direction.
                     self.set_undisprovable(&incomplete_pixels, bi);