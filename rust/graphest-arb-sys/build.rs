df416516e69f89d0f61b41e42714cf733c9da442
unageek <29035331+unageek@users.noreply.github.com>
2022-01-17 18:05:22 +0900

    Update Arb to v2.22.0

--- rust/graphest-arb-sys/build.rs
+++ rust/graphest-arb-sys/build.rs
@@ -6,7 +6,7 @@ use std::{
 
 // https://gitlab.com/tspiteri/gmp-mpfr-sys/-/blob/master/build.rs
 
-const ARB_GIT_TAG: &str = "2.21.1";
+const ARB_GIT_TAG: &str = "2.22.0";
 const ARB_GIT_URL: &str = "https://github.com/fredrik-johansson/arb.git";
 
 const FLINT_GIT_TAG: &str = "v2.8.4";
