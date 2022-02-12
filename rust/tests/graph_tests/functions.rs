5b244f146191f982914bf895d44a4ff956d3d23a
unageek <29035331+unageek@users.noreply.github.com>
2022-02-12 12:22:03 +0900

    Add regression tests for Bessel functions, Si(x), Ci(x), log(10, x)

--- rust/tests/graph_tests/functions.rs
+++ rust/tests/graph_tests/functions.rs
@@ -16,6 +16,7 @@ t!(
 );
 t!(t_e2432ca1dca74defb99c089a7d232eeb, "y = ceil(x)");
 t!(t_3c8809b76d304630b6bb047ed44c3c84, "y = Chi(x)");
+t!(t_2f00a24bfa8c4d4ab2f74382299d1029, "y = Ci(x)");
 t!(t_4e6cef6277de458aa116679f52721b31, "y = cos(x)");
 t!(t_ed14cf332fd64c8398d072b14dd93d54, "y = cosh(x)");
 t!(
@@ -35,6 +36,7 @@ t!(t_4e62c6b2dc704f47a8275b5f412b6f6c, "y = Gamma(x)");
 t!(t_4f6d9c9c7b8d484993cb265a88322897, "y = K(x)");
 t!(t_a1781b46eb8c45f58d656f455677c84c, "y = li(x)");
 t!(t_6c486f3c2aef49dead6acd55f3848433, "y = ln(x)");
+t!(t_09177d1cf2fb44af91b5e47a7213019d, "y = log(10, x)");
 t!(t_0e998089005f4afda4752098d63cd55a, "y = psi(x)");
 t!(
     t_a99a60361a8d4b69a1461484c0622e10,
@@ -43,9 +45,81 @@ t!(
 );
 t!(t_15e48b4aa9864b56bc295596008ced2f, "y = sgn(x)");
 t!(t_c6e28ae9902e4517885c39c5c776e26a, "y = Shi(x)");
+t!(t_1a93bd43905e4d7eb041e4fe75175534, "y = Si(x)");
 t!(t_b9fb18d5692e4ae6ab6e011ee31e32e6, "y = sin(x)");
 t!(t_2167a4da30594e209d49215d180c6edb, "y = sinc(x)");
 t!(t_5ac006668e084891a8394362499446ea, "y = sinh(x)");
 t!(t_58ee894ce5fb4164ac0e3c422ac7948f, "y = sqrt(x)");
 t!(t_51f6423ada6a45eba368eb03b57e10ee, "y = tan(x)");
 t!(t_c88349f5b4a74502b82b56c56ffd9ea7, "y = tanh(x)");
+
+// Bessel functions
+t!(
+    t_4f55a2d3f14b4539b5a1c38277a1aaf1,
+    "y = J(-1, x)",
+    @bounds(-4, 4, -4, 4),
+    @timeout(2000),
+);
+t!(
+    t_e657f897db644391905f6245dfd67df1,
+    "y = J(-1/2, x)",
+    @bounds(-4, 4, -4, 4),
+);
+t!(
+    t_f8780e7966644953b23ded3a6ce8e2a6,
+    "y = J(0, x)",
+    @bounds(-4, 4, -4, 4),
+    @timeout(2000),
+);
+t!(
+    t_b7e50ea47abe42559b286b66964498d7,
+    "y = J(1/2, x)",
+    @bounds(-4, 4, -4, 4),
+);
+t!(
+    t_6334b67231b84fcaa62320fa65b7c77f,
+    "y = J(1, x)",
+    @bounds(-4, 4, -4, 4),
+    @timeout(2000),
+);
+
+t!(
+    t_2bba64e6e7d24423bbd3d2a08fe5c780,
+    "y = Y(-1, x)",
+    @bounds(-4, 4, -4, 4),
+    @timeout(3000),
+);
+t!(
+    t_1d96ba0ba09f44e7a9e3f497526013fa,
+    "y = Y(-1/2, x)",
+    @bounds(-4, 4, -4, 4),
+);
+t!(
+    t_9359b338708e4df1a8bdc6220b95e527,
+    "y = Y(0, x)",
+    @bounds(-4, 4, -4, 4),
+    @timeout(2000),
+);
+t!(
+    t_795e8b2422db48ca86b5de4a2de94b4c,
+    "y = Y(1/2, x)",
+    @bounds(-4, 4, -4, 4),
+);
+t!(
+    t_a95ce899a2c24239b21bd1f787610d4b,
+    "y = Y(1, x)",
+    @bounds(-4, 4, -4, 4),
+    @timeout(3000),
+);
+
+t!(t_eaa5ae3d73ac4599a0dcb865278684f2, "y = I(-1, x)",);
+t!(t_a495cd2e3ac141729ce70da2bbf69005, "y = I(-1/2, x)",);
+t!(t_8355dc4b93bf4a8896db89c7abdb804f, "y = I(0, x)",);
+t!(t_6a5ff0062f8c46ad9bbb4c8dc7d9681c, "y = I(1/2, x)",);
+t!(t_961ac5f68d8747db8b1e701cd0138931, "y = I(1, x)",);
+
+t!(t_24fee629fc8d49008760b5da7a2122c6, "y = K(-1, x)",);
+t!(t_17e51ff3f84d463384561a781a909c5c, "y = K(-1/2, x)",);
+t!(t_52c7fee2d49a4cc7ab8500dc20bc917d, "y = K(0, x)",);
+t!(t_d90cfe5768e045d2b2f06f6a17de8f45, "y = K(1/2, x)",);
+t!(t_2a7837af7fa54a4ca9da271ac72f339b, "y = K(1, x)",);
