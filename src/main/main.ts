unageek/graphest

commit ab2c4e50988816a1472c4792adcbae74775225de

Distinguish relations by their ASTs (#524)

src/main/main.ts
@@ -29,7 +29,7 @@ import {
 import * as ipc from "../common/ipc";
 import { MenuItem } from "../common/MenuItem";
 import { Range } from "../common/range";
-import { ValidationResult } from "../common/validationResult";
+import * as result from "../common/result";
 
 const fsPromises = fs.promises;
 
@@ -99,6 +99,8 @@ let mainMenu: Menu | undefined;
 let mainWindow: BrowserWindow | undefined;
 let nextRelId = 0;
 const relationById = new Map<string, Relation>();
+const astToRelationId = new Map<string, string>();
+const astToRelationIdHighRes = new Map<string, string>();
 
 function createMainMenu(): Menu {
   // https://www.electronjs.org/docs/api/menu#examples
@@ -281,127 +283,131 @@ app.on("window-all-closed", () => {
   app.quit();
 });
 
-ipcMain.handle(ipc.abortGraphing, async (_, relId, tileId) => {
-  abortJobs(
-    (j) => j.relId === relId && (tileId === undefined || j.tileId === tileId)
-  );
-});
-
-ipcMain.handle(ipc.newRelation, async (_, rel, highRes) => {
-  const relId = nextRelId.toString();
-  nextRelId++;
-  const outDir = path.join(baseOutDir, relId);
-  await fsPromises.mkdir(outDir);
-  relationById.set(relId, {
-    highRes,
-    id: relId,
-    nextTileNumber: 0,
-    outDir,
-    rel,
-    tiles: new Map(),
-  });
-  return { relId };
-});
-
-ipcMain.handle(ipc.openUrl, async (_, url) => {
-  if (!url.startsWith("https://")) return;
-  shell.openExternal(url);
-});
-
-ipcMain.handle(ipc.requestTile, async (_, relId, tileId, coords) => {
-  const rel = relationById.get(relId);
-  if (rel === undefined) {
-    return;
-  }
-
-  const tile = rel.tiles.get(tileId);
-  const retinaScale = rel.highRes ? 2 : 1;
-  if (tile === undefined) {
-    // We offset the graph by 0.5px to place the origin at the center of a pixel.
-    // The direction of offsetting must be coherent with the configuration of `GridLayer`.
-    // We also add asymmetric perturbation to the offset so that
-    // points with simple coordinates may not be located on pixel boundaries,
-    // which could make lines such as `y = x` look thicker.
-    const pixelOffsetX = bignum(
-      (0.5 + 1.2345678901234567e-3) / (retinaScale * GRAPH_TILE_SIZE)
-    );
-    const pixelOffsetY = bignum(
-      (0.5 + 1.3456789012345678e-3) / (retinaScale * GRAPH_TILE_SIZE)
-    );
-    const widthPerTile = bignum(2 ** (BASE_ZOOM_LEVEL - coords.z));
-    const x0 = widthPerTile.times(bignum(coords.x).minus(pixelOffsetX));
-    const x1 = widthPerTile.times(bignum(coords.x + 1).minus(pixelOffsetX));
-    const y0 = widthPerTile.times(bignum(-coords.y - 1).plus(pixelOffsetY));
-    const y1 = widthPerTile.times(bignum(-coords.y).plus(pixelOffsetY));
-
-    const outFile = path.join(rel.outDir, rel.nextTileNumber + ".png");
-    rel.nextTileNumber++;
-
-    const newTile: Tile = {
-      id: tileId,
-      url: pathToFileURL(outFile).href,
-    };
-    rel.tiles.set(tileId, newTile);
-
-    const job: Job = {
-      aborted: false,
-      args: [
-        "--bounds",
-        x0.toString(),
-        x1.toString(),
-        y0.toString(),
-        y1.toString(),
-        "--size",
-        (retinaScale * EXTENDED_GRAPH_TILE_SIZE).toString(),
-        (retinaScale * EXTENDED_GRAPH_TILE_SIZE).toString(),
-        "--padding-right",
-        (retinaScale * GRAPH_TILE_EXTENSION).toString(),
-        "--padding-bottom",
-        (retinaScale * GRAPH_TILE_EXTENSION).toString(),
-        "--dilate",
-        retinaScale === 2 ? "1,1,0;1,1,0;0,0,0" : "1",
-        "--gray-alpha",
-        "--output",
-        outFile,
-        "--mem-limit",
-        JOB_MEM_LIMIT.toString(),
-        "--pause-per-output",
-        "--",
-        rel.rel,
-      ],
-      outFile,
-      relId,
-      tileId,
-    };
-    pushJob(job);
-    updateQueue();
-  } else {
-    if (tile.version !== undefined) {
-      notifyTileReady(relId, tileId, false);
-    }
-  }
+ipcMain.handle(ipc.abortGraphing, async (_, relId: string, tileId?: string) => {
+  abortJobs((j) => j.relId === relId && j.tileId === tileId);
 });
 
 ipcMain.handle(
-  ipc.validateRelation,
-  async (_, rel): Promise<ValidationResult> => {
+  ipc.requestRelation,
+  async (
+    _,
+    rel: string,
+    highRes: boolean
+  ): Promise<ipc.RequestRelationResult> => {
     try {
-      await util.promisify(execFile)(graphExec, ["--parse", "--", rel]);
+      const args = ["--parse", "--dump-ast", "--", rel];
+      const { stdout } = await util.promisify(execFile)(graphExec, args);
+      const ast = stdout.split("\n")[0];
+      let relId = highRes
+        ? astToRelationIdHighRes.get(ast)
+        : astToRelationId.get(ast);
+      if (relId === undefined) {
+        relId = nextRelId.toString();
+        nextRelId++;
+        const outDir = path.join(baseOutDir, relId);
+        await fsPromises.mkdir(outDir);
+        relationById.set(relId, {
+          highRes,
+          id: relId,
+          nextTileNumber: 0,
+          outDir,
+          rel,
+          tiles: new Map(),
+        });
+        (highRes ? astToRelationIdHighRes : astToRelationId).set(ast, relId);
+      }
+      return result.ok(relId);
     } catch ({ stderr }) {
-      if (typeof stderr === "string") {
-        const lines = stderr.split("\n");
-        const start =
-          parseInt((lines[1].match(/^.*:\d+:(\d+)/) as RegExpMatchArray)[1]) -
-          1;
-        const len = (lines[3].match(/~*$/) as RegExpMatchArray)[0].length;
-        const message = (lines[1].match(/error: (.*)$/) as RegExpMatchArray)[1];
-        return {
-          range: new Range(start, start + len),
-          message,
-        };
+      if (typeof stderr !== "string") {
+        throw new Error("`stderr` must be a string");
+      }
+      const lines = stderr.split("\n");
+      const start =
+        parseInt((lines[1].match(/^.*:\d+:(\d+)/) as RegExpMatchArray)[1]) - 1;
+      const len = (lines[3].match(/~*$/) as RegExpMatchArray)[0].length;
+      const message = (lines[1].match(/error: (.*)$/) as RegExpMatchArray)[1];
+      return result.err({
+        range: new Range(start, start + len),
+        message,
+      });
+    }
+  }
+);
+
+ipcMain.handle(
+  ipc.requestTile,
+  async (_, relId: string, tileId: string, coords: L.Coords) => {
+    const rel = relationById.get(relId);
+    if (rel === undefined) {
+      return;
+    }
+
+    const tile = rel.tiles.get(tileId);
+    const retinaScale = rel.highRes ? 2 : 1;
+    if (tile === undefined) {
+      // We offset the graph by 0.5px to place the origin at the center of a pixel.
+      // The direction of offsetting must be coherent with the configuration of `GridLayer`.
+      // We also add asymmetric perturbation to the offset so that
+      // points with simple coordinates may not be located on pixel boundaries,
+      // which could make lines such as `y = x` look thicker.
+      const pixelOffsetX = bignum(
+        (0.5 + 1.2345678901234567e-3) / (retinaScale * GRAPH_TILE_SIZE)
+      );
+      const pixelOffsetY = bignum(
+        (0.5 + 1.3456789012345678e-3) / (retinaScale * GRAPH_TILE_SIZE)
+      );
+      const widthPerTile = bignum(2 ** (BASE_ZOOM_LEVEL - coords.z));
+      const x0 = widthPerTile.times(bignum(coords.x).minus(pixelOffsetX));
+      const x1 = widthPerTile.times(bignum(coords.x + 1).minus(pixelOffsetX));
+      const y0 = widthPerTile.times(bignum(-coords.y - 1).plus(pixelOffsetY));
+      const y1 = widthPerTile.times(bignum(-coords.y).plus(pixelOffsetY));
+
+      const outFile = path.join(rel.outDir, rel.nextTileNumber + ".png");
+      rel.nextTileNumber++;
+
+      const newTile: Tile = {
+        id: tileId,
+        url: pathToFileURL(outFile).href,
+      };
+      rel.tiles.set(tileId, newTile);
+
+      const job: Job = {
+        aborted: false,
+        args: [
+          "--bounds",
+          x0.toString(),
+          x1.toString(),
+          y0.toString(),
+          y1.toString(),
+          "--size",
+          (retinaScale * EXTENDED_GRAPH_TILE_SIZE).toString(),
+          (retinaScale * EXTENDED_GRAPH_TILE_SIZE).toString(),
+          "--padding-right",
+          (retinaScale * GRAPH_TILE_EXTENSION).toString(),
+          "--padding-bottom",
+          (retinaScale * GRAPH_TILE_EXTENSION).toString(),
+          "--dilate",
+          retinaScale === 2 ? "1,1,0;1,1,0;0,0,0" : "1",
+          "--gray-alpha",
+          "--output",
+          outFile,
+          "--mem-limit",
+          JOB_MEM_LIMIT.toString(),
+          "--pause-per-output",
+          "--",
+          rel.rel,
+        ],
+        outFile,
+        relId,
+        tileId,
+      };
+      pushJob(job);
+      updateQueue();
+    } else {
+      if (tile.version !== undefined) {
+        notifyTileReady(relId, tileId, false);
       }
     }
-    return null;
   }
 );
 

src/common/ipc.ts
@@ -1,6 +1,14 @@
 import { IpcRendererEvent } from "electron";
 import { MenuItem } from "./MenuItem";
-import { ValidationResult } from "./validationResult";
+import { Range } from "./range";
+import { Result } from "./result";
+
+export interface RelationError {
+  range: Range;
+  message: string;
+}
+
+export type RequestRelationResult = Result<string, RelationError>;
 
 export interface MessageToMain {
   channel: string;
@@ -15,13 +23,6 @@ export interface AbortGraphing extends MessageToMain {
   result: void;
 }
 
-export const newRelation = "new-relation";
-export interface NewRelation extends MessageToMain {
-  channel: typeof newRelation;
-  args: [rel: string, highRes: boolean];
-  result: { relId: string };
-}
-
 export const openUrl = "open-url";
 export interface OpenUrl extends MessageToMain {
   channel: typeof openUrl;
@@ -29,18 +30,18 @@ export interface OpenUrl extends MessageToMain {
   result: void;
 }
 
+export const requestRelation = "request-relation";
+export interface RequestRelation extends MessageToMain {
+  channel: typeof requestRelation;
+  args: [rel: string, highRes: boolean];
+  result: RequestRelationResult;
+}
+
 export const requestTile = "request-tile";
 export interface RequestTile extends MessageToMain {
   channel: typeof requestTile;
   args: [relId: string, tileId: string, coords: L.Coords];
   result: void;
-}
-
-export const validateRelation = "validate-relation";
-export interface ValidateRelation extends MessageToMain {
-  channel: typeof validateRelation;
-  args: [rel: string];
-  result: ValidationResult;
 }
 
 export interface MessageToRenderer {

src/common/result.ts
@@ -1,0 +1,19 @@
+type Ok<T> = {
+  ok: T;
+  err: undefined;
+};
+
+type Err<E> = {
+  ok: undefined;
+  err: E;
+};
+
+export type Result<T, E> = Ok<T> | Err<E>;
+
+export function ok<T>(ok: T): Ok<T> {
+  return { ok, err: undefined };
+}
+
+export function err<E>(err: E): Err<E> {
+  return { ok: undefined, err };
+}

src/common/validationResult.ts
@@ -1,8 +1,0 @@
-import { Range } from "./range";
-
-export interface ValidationError {
-  range: Range;
-  message: string;
-}
-
-export type ValidationResult = ValidationError | null;

src/renderer/GraphBar.tsx
@@ -20,6 +20,7 @@ export interface GraphBarProps {
 export const GraphBar = (props: GraphBarProps): JSX.Element => {
   const dispatch = useDispatch();
   const graph = useSelector((s) => s.graphs.byId[props.graphId]);
+  const highRes = useSelector((s) => s.highRes);
   const theme = useTheme();
   const relationInputActionsRef = useRef<RelationInputActions>(null);
 
@@ -45,9 +46,10 @@ export const GraphBar = (props: GraphBarProps): JSX.Element => {
       <RelationInput
         actionsRef={relationInputActionsRef}
         grow
+        highRes={highRes}
         onEnterKeyPressed={props.focusGraphView}
-        onRelationChanged={(r) =>
-          dispatch(setGraphRelation(props.graphId, r, true))
+        onRelationChanged={(relId, rel) =>
+          dispatch(setGraphRelation(props.graphId, relId, rel, true))
         }
         processing={graph.isProcessing}
         relation={graph.relation}

src/renderer/GraphLayer.ts
@@ -14,10 +14,8 @@ declare module "leaflet" {
 
 export class GraphLayer extends L.GridLayer {
   private graph?: Graph;
-  private highRes = false;
   private onGraphingStatusChangedBound: ipc.GraphingStatusChanged["listener"];
   private onTileReadyBound: ipc.TileReady["listener"];
-  private relId?: string;
   private unsubscribeFromStore?: Unsubscribe;
 
   constructor(
@@ -46,7 +44,9 @@ export class GraphLayer extends L.GridLayer {
 
   onRemove(map: L.Map): this {
     super.onRemove(map);
-    this.abortGraphing();
+    if (this.graph) {
+      this.abortGraphing(this.graph.relId);
+    }
     window.ipcRenderer.off<ipc.GraphingStatusChanged>(
       ipc.graphingStatusChanged,
       this.onGraphingStatusChangedBound
@@ -67,7 +67,7 @@ export class GraphLayer extends L.GridLayer {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   protected createTile(coords: L.Coords, _: L.DoneCallback): HTMLElement {
     const outer = L.DomUtil.create("div", "leaflet-tile") as HTMLDivElement;
-    if (this.graph === undefined || this.relId === undefined) {
+    if (!this.graph) {
       return outer;
     }
 
@@ -87,24 +87,24 @@ export class GraphLayer extends L.GridLayer {
       inner.style.webkitMaskSize = EXTENDED_GRAPH_TILE_SIZE + "px";
       outer.appendChild(inner);
     }
+
     const tileId = this._tileCoordsToKey(coords);
     window.ipcRenderer.invoke<ipc.RequestTile>(
       ipc.requestTile,
-      this.relId,
+      this.graph.relId,
       tileId,
       coords
     );
+
     return outer;
   }
 
-  private abortGraphing(tileId?: string): void {
-    if (this.relId !== undefined) {
-      window.ipcRenderer.invoke<ipc.AbortGraphing>(
-        ipc.abortGraphing,
-        this.relId,
-        tileId
-      );
-    }
+  private abortGraphing(relId: string, tileId?: string): void {
+    window.ipcRenderer.invoke<ipc.AbortGraphing>(
+      ipc.abortGraphing,
+      relId,
+      tileId
+    );
   }
 
   private onAppStateChanged() {
@@ -113,18 +113,12 @@ export class GraphLayer extends L.GridLayer {
     const lastGraph = this.graph;
     this.graph = state.graphs.byId[this.graphId];
 
-    const lastHighRes = this.highRes;
-    this.highRes = state.highRes;
-
     if (this.graph.color !== lastGraph?.color) {
       this.updateColor();
     }
 
-    if (
-      this.graph.relation !== lastGraph?.relation ||
-      this.highRes !== lastHighRes
-    ) {
-      this.updateRelation();
+    if (this.graph.relId !== lastGraph?.relId) {
+      this.updateRelation(lastGraph?.relId);
     }
   }
 
@@ -133,19 +127,19 @@ export class GraphLayer extends L.GridLayer {
     relId,
     processing
   ) => {
-    if (this.relId === relId) {
+    if (relId === this.graph?.relId) {
       this.store.dispatch(setGraphIsProcessing(this.graphId, processing));
     }
   };
 
   private onTileReady: ipc.TileReady["listener"] = (_, relId, tileId, url) => {
-    if (this.relId === relId) {
+    if (relId === this.graph?.relId) {
       this.updateTile(tileId, url);
     }
   };
 
   private updateColor() {
-    if (this.graph === undefined) return;
+    if (!this.graph) return;
 
     for (const key in this._tiles) {
       const tile = this._tiles[key];
@@ -157,18 +151,11 @@ export class GraphLayer extends L.GridLayer {
     }
   }
 
-  private async updateRelation() {
-    if (this.graph === undefined) return;
+  private async updateRelation(oldRelId?: string) {
+    if (oldRelId !== undefined) {
+      this.abortGraphing(oldRelId);
+    }
 
-    // NB: `abortGraphing` depends on `this.relId`.
-    this.abortGraphing();
-
-    const { relId } = await window.ipcRenderer.invoke<ipc.NewRelation>(
-      ipc.newRelation,
-      this.graph.relation,
-      this.highRes
-    );
-    this.relId = relId;
     this.redraw();
   }
 

src/renderer/RelationInput.tsx
@@ -16,8 +16,8 @@ import {
   Slate,
   withReact,
 } from "slate-react";
+import { RelationError, RequestRelationResult } from "../common/ipc";
 import { Range } from "../common/range";
-import { ValidationResult } from "../common/validationResult";
 import {
   areBracketsBalanced,
   getDecorations,
@@ -25,9 +25,9 @@ import {
   isLeftBracket,
   isRightBracket,
   NormalizationRules,
+  requestRelation,
   Token,
   tokenize,
-  validateRelation,
 } from "./relationUtils";
 
 export interface RelationInputActions {
@@ -38,8 +38,9 @@ export interface RelationInputActions {
 export interface RelationInputProps {
   actionsRef?: RefObject<RelationInputActions>;
   grow?: boolean;
+  highRes: boolean;
   onEnterKeyPressed: () => void;
-  onRelationChanged: (relation: string) => void;
+  onRelationChanged: (relId: string, rel: string) => void;
   processing: boolean;
   relation: string;
   relationInputByUser: boolean;
@@ -276,8 +277,7 @@ export const RelationInput = (props: RelationInputProps) => {
   const [editor] = useState(
     withRelationEditingExtensions(withHistory(withReact(S.createEditor())))
   );
-  const [validationError, setValidationError] =
-    useState<ValidationResult>(null);
+  const [validationError, setValidationError] = useState<RelationError>();
   const [showValidationError, setShowValidationError] = useState(false);
   const [value, setValue] = useState<S.Descendant[]>([
     {
@@ -370,18 +370,18 @@ export const RelationInput = (props: RelationInputProps) => {
     S.Transforms.select(editor, end);
   }
 
+  async function updateRelationImmediately() {
+    const rel = S.Node.string(editor);
+    const result = await requestRelation(rel, props.highRes);
+    props.onRelationChanged(result.ok ?? "", rel);
+    setValidationError(result.err);
+    // For immediate use of the result.
+    return result;
+  }
+
   const updateRelation = useCallback(
-    debounce(async (): Promise<ValidationResult> => {
-      const rel = S.Node.string(editor);
-      const error = await validateRelation(rel);
-      if (!error) {
-        props.onRelationChanged(rel);
-      } else {
-        props.onRelationChanged("false");
-      }
-      setValidationError(error);
-      // For immediate use of the result.
-      return error;
+    debounce(async (): Promise<RequestRelationResult> => {
+      return updateRelationImmediately();
     }, 200),
     []
   );
@@ -396,6 +396,10 @@ export const RelationInput = (props: RelationInputProps) => {
     ReactEditor.focus(editor);
     moveCursorToTheEnd();
   }, []);
+
+  useEffect(() => {
+    updateRelationImmediately();
+  }, [props.highRes]);
 
   useEffect(() => {
     if (props.relationInputByUser) return;
@@ -460,11 +464,11 @@ export const RelationInput = (props: RelationInputProps) => {
             if (e.key === "Enter") {
               e.preventDefault();
               updateRelation();
-              updateRelation.flush()?.then((error) => {
-                if (error) {
-                  setShowValidationError(true);
-                } else {
+              updateRelation.flush()?.then((result) => {
+                if (result.ok !== undefined) {
                   props.onEnterKeyPressed();
+                } else {
+                  setShowValidationError(true);
                 }
               });
             }

src/renderer/models/app.ts
@@ -39,8 +39,9 @@ const slice = createSlice({
               color: "rgba(0, 0, 0, 0.75)",
               id,
               isProcessing: false,
-              relation: "y = sin(x)",
               relationInputByUser: false,
+              relation: "y = sin(x)",
+              relId: "",
             },
           },
           allIds: [...s.graphs.allIds, id],

commit 9fd72c7f00717af319d10363acb1a08200c72c88

Fix handling of `ipc.abortGraphing` broken by ab2c4e50988816a1472c4792adcbae74775225de

src/main/main.ts
@@ -284,7 +284,9 @@ app.on("window-all-closed", () => {
 });
 
 ipcMain.handle(ipc.abortGraphing, async (_, relId: string, tileId?: string) => {
-  abortJobs((j) => j.relId === relId && j.tileId === tileId);
+  abortJobs(
+    (j) => j.relId === relId && (tileId === undefined || j.tileId === tileId)
+  );
 });
 
 ipcMain.handle(