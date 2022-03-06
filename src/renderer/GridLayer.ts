unageek/graphest

commit 42597573feb6c023a3f2ec62fbe77e8f9b645cbb

Implement saving graphs to files (#564)

src/renderer/GridLayer.ts
@@ -1,6 +1,6 @@
 import * as L from "leaflet";
 import { bignum, BigNumber } from "../common/bignumber";
-import { BASE_ZOOM_LEVEL } from "../common/constants";
+import { BASE_ZOOM_LEVEL, GRAPH_TILE_SIZE } from "../common/constants";
 import {
   AxesRenderer,
   Bounds,
@@ -95,7 +95,7 @@ export class AxesLayer extends L.GridLayer {
   }
 
   #drawTile(tile: HTMLCanvasElement, coords: L.Coords, tileRange: L.Bounds) {
-    const widthPerTilef = 2 ** (BASE_ZOOM_LEVEL - coords.z);
+    const widthPerTilef = GRAPH_TILE_SIZE * 2 ** (BASE_ZOOM_LEVEL - coords.z);
     const [s0, s1] = sourcePoints(coords, widthPerTilef);
     const [d0, d1] = destinationPoints();
     const tx = getTransform([s0.x, s1.x], [d0.x, d1.x]);

src/common/command.ts
@@ -2,6 +2,12 @@ export enum Command {
   AbortGraphing = "abort-graphing",
   ExportImage = "export-image",
   HighResolution = "high-resolution",
+  NewDocument = "new-document",
+  Open = "open",
+  OpenFromClipboard = "open-from-clipboard",
+  Save = "save",
+  SaveAs = "save-as",
+  SaveToClipboard = "save-to-clipboard",
   ShowAxes = "show-axes",
   ShowMajorGrid = "show-major-grid",
   ShowMinorGrid = "show-minor-grid",

src/common/constants.ts
@@ -1,12 +1,17 @@
 /**
- * The zoom level where the width/height of graph tiles correspond to unit length in real coordinates.
+ * The zoom level where 1px corresponds to 1 unit length in real coordinates.
+ *
+ * In general, `2 ** (zoom - BASE_ZOOM_LEVEL)` pixels = 1 unit length,
+ * or 1px = `2 ** (BASE_ZOOM_LEVEL - zoom)` unit lengths.
+ *
+ * As Leaflet does not handle negative zoom levels, we need some positive offset.
  */
 export const BASE_ZOOM_LEVEL = 512;
 
 /**
  * The zoom level in which the graph is initially shown.
  */
-export const INITIAL_ZOOM_LEVEL = BASE_ZOOM_LEVEL - 2;
+export const INITIAL_ZOOM_LEVEL = BASE_ZOOM_LEVEL + 6;
 
 /**
  * The width/height of graph tiles in pixels.
@@ -28,6 +33,16 @@ export const GRAPH_TILE_EXTENSION = 1;
  * The sum of {@link GRAPH_TILE_SIZE} and {@link GRAPH_TILE_EXTENSION}.
  */
 export const EXTENDED_GRAPH_TILE_SIZE = GRAPH_TILE_SIZE + GRAPH_TILE_EXTENSION;
+
+/**
+ * The default graph color.
+ */
+export const DEFAULT_COLOR = "rgba(0, 78, 140, 0.8)"; // `SharedColors.cyanBlue20`
+
+/**
+ * The maximum value allowed for a pen size.
+ */
+export const MAX_PEN_SIZE = 1000;
 
 /**
  * The amount of coordinate perturbation in horizontal direction in pixels.

src/common/document.ts
@@ -1,0 +1,36 @@
+import * as Color from "color";
+import { array, InferType, number, object, string } from "yup";
+import {
+  BASE_ZOOM_LEVEL,
+  DEFAULT_COLOR,
+  INITIAL_ZOOM_LEVEL,
+  MAX_PEN_SIZE,
+} from "./constants";
+
+export const documentSchema = object({
+  center: array(number()).length(2).default([0, 0]),
+  graphs: array(
+    object({
+      color: string()
+        .transform((s) => {
+          try {
+            new Color(s);
+            return s;
+          } catch {
+            return DEFAULT_COLOR;
+          }
+        })
+        .default(DEFAULT_COLOR),
+      penSize: number()
+        .transform((x) => Math.min(Math.max(x, 0), MAX_PEN_SIZE))
+        .default(1),
+      relation: string().default(""),
+    })
+  ).required(),
+  version: number().required(),
+  zoomLevel: number()
+    .integer()
+    .default(INITIAL_ZOOM_LEVEL - BASE_ZOOM_LEVEL),
+});
+
+export type Document = InferType<typeof documentSchema>;

src/common/ipc.ts
@@ -1,5 +1,6 @@
 import { IpcRendererEvent } from "electron";
 import { Command } from "./command";
+import { Document } from "./document";
 import {
   ExportImageEntry,
   ExportImageOptions,
@@ -14,6 +15,12 @@ export interface RelationError {
 }
 
 export type RequestRelationResult = Result<string, RelationError>;
+
+export enum SaveTo {
+  Clipboard = "clipboard",
+  CurrentFile = "current-file",
+  NewFile = "new-file",
+}
 
 export interface MessageToMain {
   channel: string;
@@ -56,11 +63,25 @@ export interface OpenUrl extends MessageToMain {
   result: void;
 }
 
+export const ready = "ready";
+export interface Ready extends MessageToMain {
+  channel: typeof ready;
+  args: [];
+  result: void;
+}
+
 export const requestRelation = "request-relation";
 export interface RequestRelation extends MessageToMain {
   channel: typeof requestRelation;
   args: [rel: string, graphId: string, highRes: boolean];
   result: RequestRelationResult;
+}
+
+export const requestSave = "request-save";
+export interface RequestSave extends MessageToMain {
+  channel: typeof requestSave;
+  args: [doc: Document, to: SaveTo];
+  result: Promise<void>;
 }
 
 export const requestTile = "request-tile";
@@ -70,7 +91,14 @@ export interface RequestTile extends MessageToMain {
   result: void;
 }
 
-export const showSaveDialog = "open-save-dialog";
+export const requestUnload = "request-unload";
+export interface RequestUnload extends MessageToMain {
+  channel: typeof requestUnload;
+  args: [doc: Document];
+  result: Promise<void>;
+}
+
+export const showSaveDialog = "show-save-dialog";
 export interface ShowSaveDialog extends MessageToMain {
   channel: typeof showSaveDialog;
   args: [path: string];
@@ -109,6 +137,27 @@ export interface GraphingStatusChanged extends MessageToRenderer {
     event: IpcRendererEvent,
     ...args: GraphingStatusChanged["args"]
   ) => void;
+}
+
+export const initiateSave = "initiate-save";
+export interface InitiateSave extends MessageToRenderer {
+  channel: typeof initiateSave;
+  args: [to: SaveTo];
+  listener: (event: IpcRendererEvent, ...args: InitiateSave["args"]) => void;
+}
+
+export const initiateUnload = "initiate-unload";
+export interface InitiateUnload extends MessageToRenderer {
+  channel: typeof initiateUnload;
+  args: [];
+  listener: (event: IpcRendererEvent, ...args: InitiateUnload["args"]) => void;
+}
+
+export const load = "load";
+export interface Load extends MessageToRenderer {
+  channel: typeof load;
+  args: [doc: Document];
+  listener: (event: IpcRendererEvent, ...args: Load["args"]) => void;
 }
 
 export const tileReady = "tile-ready";

src/main/encode.ts
@@ -1,0 +1,11 @@
+import { Buffer } from "buffer";
+
+export function fromBase64Url(base64Url: string): string {
+  const buf = Buffer.from(base64Url, "base64url");
+  return buf.toString("utf8");
+}
+
+export function toBase64Url(string: string): string {
+  const buf = Buffer.from(string, "utf8");
+  return buf.toString("base64url");
+}

src/main/main.ts
@@ -1,6 +1,14 @@
 import * as assert from "assert";
-import { ChildProcess, execFile } from "child_process";
-import { app, BrowserWindow, dialog, ipcMain, Menu, shell } from "electron";
+import { ChildProcess, execFile, spawn } from "child_process";
+import {
+  app,
+  BrowserWindow,
+  clipboard,
+  dialog,
+  ipcMain,
+  Menu,
+  shell,
+} from "electron";
 import installExtension, {
   REACT_DEVELOPER_TOOLS,
   REDUX_DEVTOOLS,
@@ -20,6 +28,7 @@ import {
   PERTURBATION_X,
   PERTURBATION_Y,
 } from "../common/constants";
+import { Document } from "../common/document";
 import {
   ANTI_ALIASING_OPTIONS,
   ExportImageEntry,
@@ -30,9 +39,12 @@ import {
   MAX_EXPORT_TIMEOUT,
 } from "../common/exportImage";
 import * as ipc from "../common/ipc";
+import { SaveTo } from "../common/ipc";
 import { Range } from "../common/range";
 import * as result from "../common/result";
+import { fromBase64Url, toBase64Url } from "./encode";
 import { createMainMenu } from "./mainMenu";
+import { deserialize, serialize } from "./serialize";
 
 const fsPromises = fs.promises;
 
@@ -66,6 +78,8 @@ const MAX_ACTIVE_JOBS = 4;
 
 /** The maximum amount of memory in MiB that each running job can use. */
 const JOB_MEM_LIMIT = 64;
+
+const URL_PREFIX = "graphest://";
 
 interface Job {
   aborted: boolean;
@@ -107,13 +121,21 @@ let sleepingJobs: Job[] = [];
 
 const baseOutDir: string = fs.mkdtempSync(path.join(os.tmpdir(), "graphest-"));
 const composeExec: string = getBundledExecutable("compose");
+let currentPath: string | undefined;
 let exportImageAbortController: AbortController | undefined;
 const graphExec: string = getBundledExecutable("graph");
 const joinTilesExec: string = getBundledExecutable("join-tiles");
+let lastSavedData = '{"center":[0,0],"graphs":[],"version":1,"zoomLevel":6}';
 let mainMenu: Menu | undefined;
 let mainWindow: BrowserWindow | undefined;
+let maybeUnsaved = false;
 let nextExportImageId = 0;
 let nextRelId = 0;
+let postStartup: (() => void | Promise<void>) | undefined = () =>
+  openUrl(
+    "graphest://eyJjZW50ZXIiOlswLDBdLCJncmFwaHMiOlt7ImNvbG9yIjoicmdiYSgwLCA3OCwgMTQwLCAwLjgpIiwicGVuU2l6ZSI6MSwicmVsYXRpb24iOiJ5ID0gc2luKHgpIn1dLCJ2ZXJzaW9uIjoxLCJ6b29tTGV2ZWwiOjZ9"
+  );
+let postUnload: (() => void | Promise<void>) | undefined;
 const relationById = new Map<string, Relation>();
 const relKeyToRelId = new Map<string, string>();
 
@@ -127,9 +149,17 @@ function createMainWindow() {
       spellcheck: false,
     },
     width: 800,
-  }).on("closed", () => {
-    mainWindow = undefined;
-  });
+  })
+    .on("close", (e) => {
+      if (maybeUnsaved) {
+        postUnload = () => mainWindow?.close();
+        mainWindow?.webContents.send(ipc.initiateUnload);
+        e.preventDefault();
+      }
+    })
+    .on("closed", () => {
+      mainWindow = undefined;
+    });
   mainWindow.loadFile(path.join(__dirname, "index.html"));
 }
 
@@ -147,6 +177,17 @@ function resetBrowserZoom() {
   }
 }
 
+// https://www.electronjs.org/docs/latest/tutorial/launch-app-from-url-in-another-app
+if (process.defaultApp) {
+  if (process.argv.length >= 2) {
+    app.setAsDefaultProtocolClient("graphest", process.execPath, [
+      path.resolve(process.argv[1]),
+    ]);
+  }
+} else {
+  app.setAsDefaultProtocolClient("graphest");
+}
+
 app.whenReady().then(async () => {
   // https://github.com/MarshallOfSound/electron-devtools-installer/issues/195#issuecomment-932634933
   await installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS], {
@@ -162,6 +203,18 @@ app.whenReady().then(async () => {
     [Command.HighResolution]: () => {
       mainWindow?.webContents.send(ipc.commandInvoked, Command.HighResolution);
     },
+    [Command.NewDocument]: () => newDocument(),
+    [Command.Open]: () => open(),
+    [Command.OpenFromClipboard]: () => openFromClipboard(),
+    [Command.Save]: () => {
+      mainWindow?.webContents.send(ipc.initiateSave, SaveTo.CurrentFile);
+    },
+    [Command.SaveAs]: () => {
+      mainWindow?.webContents.send(ipc.initiateSave, SaveTo.NewFile);
+    },
+    [Command.SaveToClipboard]: () => {
+      mainWindow?.webContents.send(ipc.initiateSave, SaveTo.Clipboard);
+    },
     [Command.ShowAxes]: () => {
       mainWindow?.webContents.send(ipc.commandInvoked, Command.ShowAxes);
     },
@@ -175,6 +228,14 @@ app.whenReady().then(async () => {
   Menu.setApplicationMenu(mainMenu);
   createMainWindow();
   autoUpdater.checkForUpdatesAndNotify();
+});
+
+app.on("open-file", (_, path) => {
+  openFile(path);
+});
+
+app.on("open-url", (_, url) => {
+  openUrl(url);
 });
 
 app.on("quit", () => {
@@ -393,6 +454,11 @@ ipcMain.handle(ipc.getDefaultExportImagePath, async (): Promise<string> => {
   }
 });
 
+ipcMain.handle(ipc.ready, () => {
+  postStartup?.();
+  postStartup = undefined;
+});
+
 ipcMain.handle(
   ipc.requestRelation,
   async (
@@ -440,6 +506,10 @@ ipcMain.handle(
   }
 );
 
+ipcMain.handle(ipc.requestSave, async (_, doc: Document, to: SaveTo) => {
+  save(doc, to);
+});
+
 ipcMain.handle(
   ipc.requestTile,
   async (_, relId: string, tileId: string, coords: L.Coords) => {
@@ -462,7 +532,9 @@ ipcMain.handle(
       const pixelOffsetY = bignum(
         (0.5 + PERTURBATION_Y) / (retinaScale * GRAPH_TILE_SIZE)
       );
-      const widthPerTile = bignum(2 ** (BASE_ZOOM_LEVEL - coords.z));
+      const widthPerTile = bignum(
+        GRAPH_TILE_SIZE * 2 ** (BASE_ZOOM_LEVEL - coords.z)
+      );
       const x0 = widthPerTile.times(bignum(coords.x).minus(pixelOffsetX));
       const x1 = widthPerTile.times(bignum(coords.x + 1).minus(pixelOffsetX));
       const y0 = widthPerTile.times(bignum(-coords.y - 1).plus(pixelOffsetY));
@@ -516,6 +588,10 @@ ipcMain.handle(
     }
   }
 );
+
+ipcMain.handle(ipc.requestUnload, async (_, doc: Document): Promise<void> => {
+  unload(doc);
+});
 
 ipcMain.handle(
   ipc.showSaveDialog,
@@ -584,6 +660,24 @@ function deprioritize(job: Job) {
   updateQueue();
 }
 
+function getCurrentFilenameForDisplay(): string {
+  return getFilenameForDisplay(currentPath);
+}
+
+function getFilenameForDisplay(thePath?: string): string {
+  if (thePath !== undefined) {
+    return path.basename(thePath, ".graphest");
+  } else {
+    return "Untitled";
+  }
+}
+
+function newDocument() {
+  openUrl(
+    "graphest://eyJncmFwaHMiOlt7ImNvbG9yIjoicmdiYSgwLCA3OCwgMTQwLCAwLjgpIiwicGVuU2l6ZSI6MSwicmVsYXRpb24iOiIifV0sInZlcnNpb24iOjF9"
+  );
+}
+
 function notifyExportImageStatusChanged(progress: ExportImageProgress) {
   mainWindow?.webContents.send(ipc.exportImageStatusChanged, progress);
 }
@@ -614,6 +708,83 @@ function notifyTileReady(
   );
 }
 
+async function open() {
+  if (!mainWindow) return;
+
+  const result = await dialog.showOpenDialog(mainWindow, {
+    defaultPath: currentPath,
+    filters: [{ name: "Graphest Document", extensions: ["graphest"] }],
+  });
+  if (result.filePaths.length !== 1) return;
+  const path = result.filePaths[0];
+
+  openFile(path);
+}
+
+async function openFile(path: string) {
+  if (!mainWindow) {
+    postStartup = () => openFile(path);
+    return;
+  }
+
+  if (maybeUnsaved) {
+    postUnload = () => openFile(path);
+    mainWindow.webContents.send(ipc.initiateUnload);
+    return;
+  }
+
+  try {
+    const data = await fsPromises.readFile(path, { encoding: "utf8" });
+    const doc = deserialize(data);
+    currentPath = path;
+    lastSavedData = data;
+    maybeUnsaved = true;
+    mainWindow.setRepresentedFilename(path);
+    mainWindow.setTitle(getCurrentFilenameForDisplay());
+    mainWindow.webContents.send(ipc.load, doc);
+  } catch (e) {
+    console.log("open failed", e);
+    dialog.showMessageBox(mainWindow, {
+      message:
+        `The document “${getFilenameForDisplay(path)}”` +
+        " could not be opened.",
+      type: "warning",
+    });
+  }
+}
+
+function openFromClipboard() {
+  openUrl(clipboard.readText());
+}
+
+function openUrl(url: string) {
+  if (!mainWindow) {
+    postStartup = () => openUrl(url);
+    return;
+  }
+
+  if (maybeUnsaved) {
+    postUnload = () => openUrl(url);
+    mainWindow.webContents.send(ipc.initiateUnload);
+    return;
+  }
+
+  if (url.startsWith(URL_PREFIX)) {
+    try {
+      const data = fromBase64Url(url.substring(URL_PREFIX.length));
+      const doc = deserialize(data);
+      currentPath = undefined;
+      lastSavedData = data;
+      maybeUnsaved = true;
+      mainWindow.setRepresentedFilename("");
+      mainWindow.setTitle(getCurrentFilenameForDisplay());
+      mainWindow.webContents.send(ipc.load, doc);
+    } catch (e) {
+      console.log("open failed", e);
+    }
+  }
+}
+
 function popJob(job: Job) {
   const nBefore = countJobs();
   queuedJobs = queuedJobs.filter((j) => j !== job);
@@ -625,6 +796,76 @@ function popJob(job: Job) {
 
 function pushJob(job: Job) {
   queuedJobs.push(job);
+}
+
+async function save(doc: Document, to: SaveTo): Promise<boolean> {
+  if (!mainWindow) return false;
+
+  const data = serialize(doc);
+
+  if (to === SaveTo.Clipboard) {
+    clipboard.writeText(URL_PREFIX + toBase64Url(data));
+  } else {
+    let path = currentPath;
+    if (to === SaveTo.NewFile || path === undefined) {
+      const result = await dialog.showSaveDialog(mainWindow, {
+        defaultPath: path,
+        filters: [{ name: "Graphest Document", extensions: ["graphest"] }],
+      });
+      if (!result.filePath) return false;
+      path = result.filePath;
+    }
+
+    try {
+      await fsPromises.writeFile(path, data);
+      if (process.platform === "darwin") {
+        // Hide filename extension.
+        await spawn("SetFile", ["-a", "E", path]);
+      }
+      currentPath = path;
+      lastSavedData = data;
+      maybeUnsaved = false;
+      mainWindow.setRepresentedFilename(path);
+      mainWindow.setTitle(getCurrentFilenameForDisplay());
+    } catch (e) {
+      console.log("save failed", e);
+      await dialog.showMessageBox({
+        type: "warning",
+        message:
+          `The document “${getCurrentFilenameForDisplay()}” could not be saved` +
+          (to === SaveTo.CurrentFile
+            ? "."
+            : ` as “${getFilenameForDisplay(path)}”.`),
+      });
+    }
+  }
+
+  return true;
+}
+
+async function unload(doc: Document) {
+  const data = serialize(doc);
+
+  if (data === lastSavedData) {
+    maybeUnsaved = false;
+    postUnload?.();
+    postUnload = undefined;
+    return;
+  }
+
+  const result = await dialog.showMessageBox({
+    message: `Do you want to save the changes made to the document “${getCurrentFilenameForDisplay()}”?`,
+    detail: "Your changes will be lost if you don't save them.",
+    buttons: ["Save…", "Don't Save", "Cancel"],
+  });
+  if (
+    (result.response === 0 && (await save(doc, SaveTo.CurrentFile))) ||
+    result.response === 1
+  ) {
+    maybeUnsaved = false;
+    postUnload?.();
+    postUnload = undefined;
+  }
 }
 
 function updateQueue() {

commit 3df78a6dc23f0aae45a6dac39466a80c0623e3d8

Fix grid interval broken in 42597573feb6c023a3f2ec62fbe77e8f9b645cbb

src/renderer/GridLayer.ts
@@ -245,7 +245,7 @@ export class GridLayer extends L.GridLayer {
     outer.appendChild(inner);
 
     setTimeout(() => {
-      const widthPerTilef = 2 ** (BASE_ZOOM_LEVEL - coords.z);
+      const widthPerTilef = GRAPH_TILE_SIZE * 2 ** (BASE_ZOOM_LEVEL - coords.z);
       const [s0, s1] = sourcePoints(coords, widthPerTilef);
       const [d0, d1] = destinationPoints();
       const tx = getTransform([s0.x, s1.x], [d0.x, d1.x]);