f74c7fec0f104f409246ba96ae0ef3c05dc1ac12
unageek <29035331+unageek@users.noreply.github.com>
2022-03-08 22:59:45 +0900

    Stricter number parsing

--- src/common/parse.ts
+++ src/common/parse.ts
@@ -15,17 +15,20 @@ export const tryParseIntegerInRange = (
   min: number,
   max: number
 ): Result<number, string> => {
-  const val = Number.parseInt(value);
-  if (val >= min && val <= max) {
+  const val = Number(value);
+  if (/^\s*[+-]?\d+\s*$/.test(value) && val >= min && val <= max) {
     return ok(val);
   } else {
-    return err(`Value must be an integer between 1 and ${max}.`);
+    return err(`Value must be an integer between ${min} and ${max}.`);
   }
 };
 
 export const tryParseNumber = (value: string): Result<number, string> => {
-  const val = Number.parseFloat(value);
-  if (Number.isFinite(val)) {
+  const val = Number(value);
+  if (
+    /^\s*[+-]?(\d+(\.\d*)?|\.\d+)([Ee][+-]?\d+)?\s*$/.test(value) &&
+    Number.isFinite(val)
+  ) {
     return ok(val);
   } else {
     return err(`Value must be a number.`);
