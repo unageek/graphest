3afb88784e81710560ced76650253e63f7216ce5
unageek <29035331+unageek@users.noreply.github.com>
2022-05-02 14:55:38 +0900

    Add doc comments

--- src/common/exportImage.ts
+++ src/common/exportImage.ts
@@ -19,26 +19,42 @@ export const MAX_EXPORT_IMAGE_SIZE = 16384;
 export const MAX_EXPORT_TIMEOUT = 300000;
 
 export interface ExportImageEntry {
+  /** The color of the graph as a case-insensitive hex code (`#RRGGBB`). */
   color: string;
+  /** The pen size of the graph in pixels. */
   penSize: number;
+  /** The ID of the relation. */
   relId: string;
 }
 
 export interface ExportImageOptions {
+  /** The level of anti-aliasing. */
   antiAliasing: number;
+  /** The height of the image in pixels. */
   height: number;
+  /** The path where the image will be saved. */
   path: string;
+  /** The timeout in milliseconds. */
   timeout: number;
+  /** Make the image background transparent. */
   transparent: boolean;
+  /** The width of the image in pixels. */
   width: number;
+  /** The right bound of the graph. */
   xMax: string;
+  /** The left bound of the graph. */
   xMin: string;
+  /** The top bound of the graph. */
   yMax: string;
+  /** The bottom bound of the graph. */
   yMin: string;
 }
 
 export interface ExportImageProgress {
+  /** The standard error output of the last-saved tile. */
   lastStderr: string;
+  /** The URL of the last-saved tile. */
   lastUrl: string;
+  /** The ratio of complete tiles (0 to 1). */
   progress: number;
 }
